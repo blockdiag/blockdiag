a2e50ce39f146035f049854cd3472ee88ae5bcb5
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2010-09-20 12:15:41 +0900

    * Implement NodeMetrix class to control metrix of nodes.

--- src/blockdiag/DiagramDraw.py
+++ src/blockdiag/DiagramDraw.py
@@ -4,7 +4,10 @@
 import Image
 import ImageFont
 import ImageDraw
+from collections import namedtuple
 
+
+XY = namedtuple('XY', 'x y')
 
 class FoldedTextDraw(ImageDraw.ImageDraw):
     def __init__(self, im, mode=None):
@@ -77,7 +80,7 @@ class FoldedTextDraw(ImageDraw.ImageDraw):
         return lines
 
 
-class NodeMetrix:
+class DiagramMetrix:
     def __init__(self, **kwargs):
         self.cellSize = kwargs.get('cellSize', 8)
         self.pageMargin = kwargs.get('pageMargin', 2)
@@ -93,46 +96,8 @@ class NodeMetrix:
         self.spanWidth = self.cellSize * self.spanColumns
         self.spanHeight = self.cellSize * self.spanRows
 
-    def nodeCoreBox(self, xy):
-        xy = self.topLeft(xy)
-        return (xy[0] + self.nodePadding,
-                xy[1] + self.nodePadding,
-                xy[0] + self.nodeWidth - self.nodePadding * 2,
-                xy[1] + self.nodeHeight - self.nodePadding * 2)
-
-    def topLeft(self, xy):
-        x = self.pageMargin + xy[0] * (self.nodeWidth + self.spanWidth)
-        y = self.pageMargin + xy[1] * (self.nodeHeight + self.spanHeight)
-
-        return (x, y)
-
-    def topCenter(self, xy):
-        x, y = self.topLeft(xy)
-        return (x + self.nodeWidth / 2, y)
-
-    def topRight(self, xy):
-        x, y = self.topLeft(xy)
-        return (x + self.nodeWidth, y)
-
-    def bottomLeft(self, xy):
-        x, y = self.topLeft(xy)
-        return (x, y + self.nodeHeight)
-
-    def bottomCenter(self, xy):
-        x, y = self.topLeft(xy)
-        return (x + self.nodeWidth / 2, y + self.nodeHeight)
-
-    def bottomRight(self, xy):
-        x, y = self.topLeft(xy)
-        return (x + self.nodeWidth, y + self.nodeHeight)
-
-    def leftCenter(self, xy):
-        x, y = self.topLeft(xy)
-        return (x, y + self.nodeHeight / 2)
-
-    def rightCenter(self, xy):
-        x, y = self.topLeft(xy)
-        return (x + self.nodeWidth, y + self.nodeHeight / 2)
+    def node(self, node):
+        return NodeMetrix(node, self)
 
     def pageSize(self, nodelist):
         x = 0
@@ -143,14 +108,85 @@ class NodeMetrix:
             if y <= node.xy[1]:
                 y = node.xy[1]
 
-        x, y = self.bottomRight((x, y))
+        x, y = NodeMetrix.new2(x, y, self).bottomRight()
         return (x + self.pageMargin, y + self.pageMargin)
+
+
+class NodeMetrix:
+    @classmethod
+    def new2(klass, x, y, metrix):
+        o = klass(None, metrix)
+        (o.x, o.y) = klass._topLeft(x, y, metrix)
+
+        return o
+
+    def __init__(self, node, metrix):
+        self.metrix = metrix
+
+        if node:
+            (self.x, self.y) = self._topLeft(node.xy[0], node.xy[1], metrix)
+
+    @classmethod
+    def _topLeft(klass, x, y, metrix):
+        x = metrix.pageMargin + x * (metrix.nodeWidth + metrix.spanWidth)
+        y = metrix.pageMargin + y * (metrix.nodeHeight + metrix.spanHeight)
+
+        return XY(x, y)
+
+    def coreBox(self):
+        m = self.metrix
+        x, y = self.topLeft()
+
+        return (x + m.nodePadding,
+                y + m.nodePadding,
+                x + m.nodeWidth - m.nodePadding * 2,
+                y + m.nodeHeight - m.nodePadding * 2)
+
+    def topLeft(self):
+        return XY(self.x, self.y)
+
+    def topCenter(self):
+        x, y = self.topLeft()
+        return XY(x + self.metrix.nodeWidth / 2, y)
+
+    def topRight(self):
+        x, y = self.topLeft()
+        return XY(x + self.metrix.nodeWidth, y)
+
+    def bottomLeft(self):
+        x, y = self.topLeft()
+        return XY(x, y + self.metrix.nodeHeight)
+
+    def bottomCenter(self):
+        m = self.metrix
+        x, y = self.topLeft()
+        return XY(x + m.nodeWidth / 2, y + m.nodeHeight)
+
+    def bottomRight(self):
+        m = self.metrix
+        x, y = self.topLeft()
+        return XY(x + m.nodeWidth, y + m.nodeHeight)
+
+    def leftCenter(self):
+        x, y = self.topLeft()
+        return XY(x, y + self.metrix.nodeHeight / 2)
+
+    def rightCenter(self):
+        m = self.metrix
+        x, y = self.topLeft()
+        return XY(x + m.nodeWidth, y + m.nodeHeight / 2)
+
+    # method aliases
+    top = topCenter;
+    bottom = bottomCenter;
+    right = rightCenter;
+    left = leftCenter;
 
 
 class DiagramDraw(ImageDraw.ImageDraw):
     def __init__(self, im, mode=None, **kwargs):
         self.image = im
-        self.metrix = NodeMetrix(**kwargs)
+        self.metrix = DiagramMetrix(**kwargs)
         self.lineSpacing = kwargs.get('lineSpacing', 2)
         self.fill = kwargs.get('fill', (0, 0, 0))
 
@@ -162,14 +198,13 @@ class DiagramDraw(ImageDraw.ImageDraw):
     def screennode(self, node, **kwargs):
         ttfont = kwargs.get('font')
 
-        top_left = self.metrix.topLeft(node.xy)
-        bottom_right = self.metrix.bottomRight(node.xy)
+        metrix = self.metrix.node(node)
         if node.color:
-            self.rectangle([top_left, bottom_right], outline=self.fill, fill=node.color)
+            self.rectangle([metrix.topLeft(), metrix.bottomRight()], outline=self.fill, fill=node.color)
         else:
-            self.rectangle([top_left, bottom_right], outline=self.fill)
+            self.rectangle([metrix.topLeft(), metrix.bottomRight()], outline=self.fill)
 
-        box = self.metrix.nodeCoreBox(node.xy)
+        box = self.metrix.node(node).coreBox()
         draw = FoldedTextDraw(self.image)
         draw.text(box, node.label, font=ttfont, lineSpacing=self.lineSpacing)
 
@@ -184,72 +219,65 @@ class DiagramDraw(ImageDraw.ImageDraw):
         spanWidth = self.metrix.spanWidth
         spanHeight = self.metrix.spanHeight
 
-        node1_xy = self.metrix.topLeft(edge.node1.xy)
-        node2_xy = self.metrix.topLeft(edge.node2.xy)
+        node1 = self.metrix.node(edge.node1)
+        node2 = self.metrix.node(edge.node2)
 
-        if edge.node1.xy[0] < edge.node2.xy[0]:
+        if node1.x < node2.x:
             # draw arrow line
-            node1_right = self.metrix.rightCenter(edge.node1.xy)
-            node2_left = self.metrix.leftCenter(edge.node2.xy)
-            node1_bottomRight = self.metrix.bottomRight(edge.node1.xy)
-            node2_bottomRight = self.metrix.bottomRight(edge.node2.xy)
-            lines.append(node1_right)
+            lines.append(node1.right())
 
             if edge.node1.xy[1] != edge.node2.xy[1]:
-                lines.append((node1_right[0] + spanWidth / 2, node1_right[1]))
-                lines.append((node1_right[0] + spanWidth / 2, node2_left[1]))
-                lines.append((node2_left[0] - spanWidth / 2, node2_left[1]))
+                lines.append((node1.right().x + spanWidth / 2, node1.right().y))
+                lines.append((node1.right().x + spanWidth / 2, node1.right().y))
+                lines.append((node1.right().x + spanWidth / 2, node2.left().y))
+                lines.append((node2.left().x - spanWidth / 2, node2.left().y))
             elif edge.node1.xy[0] + 1 < edge.node2.xy[0]:
-                lines.append((node1_right[0] + spanWidth / 2, node1_right[1]))
-                lines.append((node1_right[0] + spanWidth / 2,
-                              node1_bottomRight[1] + spanHeight / 2))
-                lines.append((node2_left[0] - spanWidth / 2,
-                              node2_bottomRight[1] + spanHeight / 2))
-                lines.append((node2_left[0] - spanWidth / 2, node2_left[1]))
+                lines.append((node1.right().x + spanWidth / 2, node1.right().y))
+                lines.append((node1.right().x + spanWidth / 2,
+                              node1.bottomRight().y + spanHeight / 2))
+                lines.append((node2.left().x - spanWidth / 2,
+                              node2.bottomRight().y + spanHeight / 2))
+                lines.append((node2.left().x - spanWidth / 2, node2.left().y))
 
-            lines.append(node2_left)
+            lines.append(node2.left())
 
             # draw arrow head
-            head.append(node2_left)
-            head.append((node2_left[0] - cellSize,
-                         node2_left[1] - cellSize / 2))
-            head.append((node2_left[0] - cellSize,
-                         node2_left[1] + cellSize / 2))
-        elif edge.node1.xy[0] == edge.node2.xy[0] and edge.node1.xy[1] > edge.node2.xy[1]:
+            head.append(node2.left())
+            head.append((node2.left().x - cellSize,
+                         node2.left().y - cellSize / 2))
+            head.append((node2.left().x - cellSize,
+                         node2.left().y + cellSize / 2))
+
+        elif node1.x == node2.x and node1.y > node2.y:
             # draw arrow line
-            node1_top = self.metrix.topCenter(node1.xy)
-            node2_bottom = self.metrix.bottomCenter(node2.xy)
-
-            lines.append(node1_top)
-            lines.append(node2_bottom)
+            lines.append(node1.top())
+            lines.append(node2.bottom())
 
             # draw arrow head
-            head.append(node2_bottom)
-            head.append((node2_bottom[0] - cellSize / 2,
-                         node2_bottom[1] + cellSize))
-            head.append((node2_bottom[0] + cellSize / 2,
-                         node2_bottom[1] + cellSize))
-        elif edge.node1.xy[1] >= edge.node2.xy[1]:
+            head.append(node2.bottom())
+            head.append((node2.bottom().x - cellSize / 2,
+                         node2.bottom().y + cellSize))
+            head.append((node2.bottom().x + cellSize / 2,
+                         node2.bottom().y + cellSize))
+
+        elif node1.y >= node2.y:
             # draw arrow line
-            node1_right = self.metrix.rightCenter(edge.node1.xy)
-            node2_top = self.metrix.topCenter(edge.node2.xy)
+            lines.append(node1.right())
+            lines.append((node1.right().x + spanWidth / 8, node1.right().y))
+            lines.append((node1.right().x + spanWidth / 8,
+                          node2.top().y - spanHeight / 2))
+            lines.append((node2.top().x, node2.top().y - spanHeight / 2))
 
-            lines.append(node1_right)
-            lines.append((node1_right[0] + spanWidth / 8, node1_right[1]))
-            lines.append((node1_right[0] + spanWidth / 8,
-                          node2_top[1] - spanHeight / 2))
-            lines.append((node2_top[0], node2_top[1] - spanHeight / 2))
-
-            lines.append(node2_top)
+            lines.append(node2.top())
 
             # draw arrow head
-            head.append(node2_top)
-            head.append((node2_top[0] - cellSize / 2,
-                         node2_top[1] - cellSize))
-            head.append((node2_top[0] + cellSize / 2,
-                         node2_top[1] - cellSize))
+            head.append(node2.top())
+            head.append((node2.top().x - cellSize / 2,
+                         node2.top().y - cellSize))
+            head.append((node2.top().x + cellSize / 2,
+                         node2.top().y - cellSize))
         else:
-            pos = node1_xy + node2_xy
+            pos = (node1.x, node1.y, node2.x, node2.y)
             raise RuntimeError, "Invalid edge: (%d, %d), (%d, %d)" % pos
 
         if edge.color:
