3cbbb859701b71d3e912ba5648a3acb8d2f03c3d
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2010-09-21 21:59:32 +0900

    * Refactored; drop ScreenNodeBuilder.forwardLinks.

---
+++ src/blockdiag/blockdiag.py
@@ -0,0 +1,222 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+import os
+import sys
+import re
+from optparse import OptionParser
+import Image
+import ImageFont
+import DiagramDraw
+import diagparser
+
+
+class ScreenNode:
+    def __init__(self, id):
+        self.id = id
+        self.label = re.sub('^"?(.*?)"?$', '\\1', id)
+        self.xy = (0, 0)
+        self.color = None
+        self.children = None
+
+    def setAttributes(self, attrs):
+        for attr in attrs:
+            value = re.sub('^"?(.*?)"?$', '\\1', attr.value)
+            if attr.name == 'label':
+                self.label = value
+            elif attr.name == 'color':
+                self.color = value
+            else:
+                msg = "Unknown node attribute: %s.%s" % (self.id, attr.name)
+                raise AttributeError(msg)
+
+
+class ScreenEdge:
+    def __init__(self, node1, node2):
+        self.node1 = node1
+        self.node2 = node2
+        self.color = None
+        self.dir = 'forward'
+
+    def setAttributes(self, attrs):
+        for attr in attrs:
+            value = re.sub('^"?(.*?)"?$', '\\1', attr.value)
+            if attr.name == 'color':
+                self.color = value
+            elif attr.name == 'dir':
+                dir = value.lower()
+                if dir in ('back', 'both', 'none'):
+                    self.dir = dir
+                else:
+                    self.dir = 'forward'
+            else:
+                raise AttributeError("Unknown edge attribute: %s" % attr.name)
+
+
+class ScreenNodeBuilder:
+    @classmethod
+    def build(klass, tree):
+        return klass()._build(tree)
+
+    def __init__(self):
+        self.uniqNodes = {}
+        self.nodeOrder = []
+        self.uniqLinks = {}
+        self.widthRefs = []
+        self.heightRefs = []
+        self.rows = 0
+
+    def _build(self, tree):
+        self.buildNodeList(tree)
+
+        return (self.uniqNodes.values(), self.uniqLinks.values())
+
+    def getScreenNode(self, id):
+        if id in self.uniqNodes:
+            node = self.uniqNodes[id]
+        else:
+            node = ScreenNode(id)
+            self.uniqNodes[id] = node
+            self.nodeOrder.append(id)
+
+        return node
+
+    def getScreenEdge(self, id1, id2):
+        link = (self.getScreenNode(id1), self.getScreenNode(id2))
+
+        if link in self.uniqLinks:
+            edge = self.uniqLinks[link]
+        else:
+            edge = ScreenEdge(link[0], link[1])
+            self.uniqLinks[link] = edge
+
+        return edge
+
+    def getChildrenIds(self, node):
+        if isinstance(node, ScreenNode):
+            node_id = node.id
+        else:
+            node_id = node
+
+        uniq = {}
+        for edge in self.uniqLinks.values():
+            if node_id == None:
+                uniq[edge.node1.id] = 1
+            elif edge.node1.id == node_id:
+                uniq[edge.node2.id] = 1
+        children = uniq.keys()
+
+        order = self.nodeOrder
+        children.sort(lambda x, y: cmp(order.index(x), order.index(y)))
+
+        return children
+
+    def setNodeWidth(self, parent, node):
+        if node.id in self.widthRefs or parent == node:
+            return
+
+        node.xy = (parent.xy[0] + 1, node.xy[1])
+        self.widthRefs.append(parent.id)
+
+        if node.id in self.getChildrenIds(None):
+            for child_id in self.getChildrenIds(node):
+                childnode = self.getScreenNode(child_id)
+                self.setNodeWidth(node, childnode)
+
+    def setNodeHeight(self, node, height):
+        node.xy = (node.xy[0], height)
+        if node.id in self.getChildrenIds(None):
+            for child_id in self.getChildrenIds(node):
+                if not child_id in self.heightRefs:
+                    childnode = self.getScreenNode(child_id)
+
+                    if node.xy[0] < childnode.xy[0]:
+                        height = self.setNodeHeight(childnode, height)
+                    else:
+                        height += 1
+
+                    self.heightRefs.append(child_id)
+                else:
+                    if not node.id in self.heightRefs:
+                        height += 1
+        else:
+            height += 1
+
+        return height
+
+    def buildNodeList(self, tree):
+        for stmt in tree.stmts:
+            if isinstance(stmt, diagparser.Node):
+                node = self.getScreenNode(stmt.id)
+                node.setAttributes(stmt.attrs)
+            elif isinstance(stmt, diagparser.Edge):
+                while len(stmt.nodes) >= 2:
+                    edge = self.getScreenEdge(stmt.nodes.pop(0), stmt.nodes[0])
+                    edge.setAttributes(stmt.attrs)
+            else:
+                raise AttributeError("Unknown sentense: " + str(type(stmt)))
+
+        for parent_id in self.getChildrenIds(None):
+            parent = self.getScreenNode(parent_id)
+            for child_id in self.getChildrenIds(parent):
+                childnode = self.getScreenNode(child_id)
+                self.setNodeWidth(parent, childnode)
+
+        height = 0
+        for node_id in self.nodeOrder:
+            node = self.uniqNodes[node_id]
+            if node.xy[0] == 0:
+                height = self.setNodeHeight(node, height)
+
+
+def main():
+    usage = "usage: %prog [options] infile"
+    p = OptionParser(usage=usage)
+    p.add_option('-o', dest='filename',
+                 help='write diagram to FILE', metavar='FILE')
+    p.add_option('-f', '--font', dest='font',
+                 help='use FONT to draw diagram', metavar='FONT')
+    (options, args) = p.parse_args()
+
+    if len(args) == 0:
+        p.print_help()
+        exit(0)
+
+    fonts = [options.font,
+             'c:/windows/fonts/VL-Gothic-Regular.ttf',
+             'c:/windows/fonts/msmincho.ttf',
+             '/usr/share/fonts/truetype/ipafont/ipagp.ttf']
+
+    ttfont = None
+    for path in fonts:
+        if path and os.path.isfile(path):
+            ttfont = ImageFont.truetype(path, 11)
+            break
+
+    draw = DiagramDraw.DiagramDraw()
+
+    infile = args[0]
+    if options.filename:
+        outfile = options.filename
+    else:
+        outfile = re.sub('\..*', '', infile) + '.png'
+
+    try:
+        tree = diagparser.parse_file(infile)
+        nodelist, edgelist = ScreenNodeBuilder.build(tree)
+
+        draw.screennodelist(nodelist, font=ttfont)
+        draw.edgelist(edgelist)
+    except Exception, e:
+        import traceback
+        traceback.print_exc()
+
+        name = e.__class__.__name__
+        print "[%s] %s" % (name, e)
+        exit(1)
+
+    draw.save(outfile, 'PNG')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/blockdiag/tests/test_parser.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+
+import tempfile
+from blockdiag.blockdiag import *
+from blockdiag.diagparser import *
+from nose.tools import assert_raises
+
+
+def test_diagparser():
+    # basic digram
+    str = ("diagram test {\n"
+           "}\n")
+
+    tree = parse(tokenize(str))
+    assert isinstance(tree, Graph)
+
+    # digram without id
+    str = ("diagram {\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    assert isinstance(tree, Graph)
+
+    # digram without diagram and id
+    str = ("{\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    assert isinstance(tree, Graph)
+
+    # parsing error for empty string
+    def dummy():
+        str = ""
+        tree = parse(tokenize(str))
+    assert_raises(NoParseError, dummy)
+
+
+def test_empty_diagram():
+    # empty diagram
+    str = ("diagram {\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    nodelist, edgelist = ScreenNodeBuilder.build(tree)
+
+    assert len(nodelist) == 0
+    assert len(edgelist) == 0
+
+
+def test_single_node_diagram():
+    # empty diagram
+    str = ("diagram {\n"
+           "  A;\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    nodelist, edgelist = ScreenNodeBuilder.build(tree)
+
+    assert len(nodelist) == 1
+    assert len(edgelist) == 0
+    assert nodelist[0].label == 'A'
+    assert nodelist[0].xy == (0, 0)
+
+
+def test_single_edge_diagram():
+    # empty diagram
+    str = ("diagram {\n"
+           "  A -> B;\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    nodelist, edgelist = ScreenNodeBuilder.build(tree)
+
+    assert len(nodelist) == 2
+    assert len(edgelist) == 1
+    assert nodelist[0].label == 'A'
+    assert nodelist[1].label == 'B'
+
+    assert_pos = {'A': (0, 0), 'B': (1, 0)}
+    assert_label = {'A': 'A', 'B': 'B'}
+    for node in nodelist:
+        assert node.xy == assert_pos[node.id]
+        assert node.label == assert_label[node.id]
+
+
+def test_two_edges_diagram():
+    # empty diagram
+    str = ("diagram {\n"
+           "  A -> B -> C;\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    nodelist, edgelist = ScreenNodeBuilder.build(tree)
+
+    assert len(nodelist) == 3
+    assert len(edgelist) == 2
+
+    assert_pos = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0)}
+    for node in nodelist:
+        assert node.xy == assert_pos[node.id]
+
+
+def test_node_attribute():
+    # empty diagram
+    str = ("diagram {\n"
+           "  foo [label = bar, color = red];\n"
+           "  bar\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    nodelist, edgelist = ScreenNodeBuilder.build(tree)
+
+    assert nodelist[0].id == 'foo'
+    assert nodelist[0].label == 'bar'
+    assert nodelist[0].color == 'red'
+    assert nodelist[0].xy == (0, 0)
+
+    assert nodelist[1].id == 'bar'
+    assert nodelist[1].label == 'bar'
+    assert nodelist[1].color == None
+    assert nodelist[1].xy == (0, 1)
+
+
+def test_edge_attribute():
+    # empty diagram
+    str = ("diagram {\n"
+           "  A -> B -> C [color = red]\n"
+           "  D -> E [dir = none]\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    nodelist, edgelist = ScreenNodeBuilder.build(tree)
+
+    assert_pos = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0),
+                  'D': (0, 1), 'E': (1, 1)}
+    for node in nodelist:
+        assert node.xy == assert_pos[node.id]
+
+    for edge in edgelist:
+        if edge.node1.id == 'D':
+            assert edge.dir == 'none'
+            assert edge.color == None
+        else:
+            assert edge.dir == 'forward'
+            assert edge.color == 'red'
+
+
+def test_branched_diagram():
+    # empty diagram
+    str = ("diagram {\n"
+           "  A -> B -> C\n"
+           "  A -> D -> E\n"
+           "  Z\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    nodelist, edgelist = ScreenNodeBuilder.build(tree)
+
+    assert_pos = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0),
+                  'D': (1, 1), 'E': (2, 1), 'Z': (0, 2)}
+    for node in nodelist:
+        assert node.xy == assert_pos[node.id]
+
+
+def test_circular_ref_diagram():
+    # empty diagram
+    str = ("diagram {\n"
+           "  A -> B -> C -> A\n"
+           "       B -> D\n"
+           "  Z\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    nodelist, edgelist = ScreenNodeBuilder.build(tree)
+
+    assert_pos = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0),
+                  'D': (2, 1), 'Z': (0, 2)}
+    for node in nodelist:
+        assert node.xy == assert_pos[node.id]
+
+
+def test_skipped_edge_diagram():
+    # empty diagram
+    str = ("diagram {\n"
+           "  A -> B -> C -> A\n"
+           "  A      -> C\n"
+           "  Z\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    nodelist, edgelist = ScreenNodeBuilder.build(tree)
+
+    assert_pos = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0), 'Z': (0, 1)}
+    for node in nodelist:
+        assert node.xy == assert_pos[node.id]
+
+
+def test_triple_branched_diagram():
+    # empty diagram
+    str = ("diagram {\n"
+           "  A -> D\n"
+           "  B -> D\n"
+           "  C -> D\n"
+           "  Z\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    nodelist, edgelist = ScreenNodeBuilder.build(tree)
+
+    assert_pos = {'A': (0, 0), 'B': (0, 1), 'C': (0, 2),
+                  'D': (1, 0), 'Z': (0, 3)}
+    for node in nodelist:
+        assert node.xy == assert_pos[node.id]
+
+
+def test_twin_circular_ref_diagram():
+    # empty diagram
+    str = ("diagram {\n"
+           "  A -> B -> A\n"
+           "  A -> C -> A\n"
+           "  Z\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    nodelist, edgelist = ScreenNodeBuilder.build(tree)
+
+    assert_pos = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1), 'Z': (0, 2)}
+    for node in nodelist:
+        assert node.xy == assert_pos[node.id]
+
+
+def test_self_ref_diagram():
+    # empty diagram
+    str = ("diagram {\n"
+           "  A -> B -> B\n"
+           "  Z\n"
+           "}\n")
+    tree = parse(tokenize(str))
+    nodelist, edgelist = ScreenNodeBuilder.build(tree)
+
+    assert_pos = {'A': (0, 0), 'B': (1, 0), 'Z': (0, 1)}
+    for node in nodelist:
+        assert node.xy == assert_pos[node.id]
