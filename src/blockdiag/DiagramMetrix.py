5bea4831bbb06e4a6328c831bdec48c5b085e322
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2010-09-26 17:20:46 +0900

    * Move DiagramDraw.lineSpacing to DiagramMetrix class.  * Refacored; DiagramMetrix#__init__().

--- src/blockdiag/DiagramMetrix.py
+++ src/blockdiag/DiagramMetrix.py
@@ -21,20 +21,19 @@ class xylist(list):
 class DiagramMetrix:
     def __init__(self, **kwargs):
         self.cellSize = kwargs.get('cellSize', 8)
-        self.pageMargin = kwargs.get('pageMargin', 3)
         self.nodePadding = kwargs.get('nodePadding', 4)
-        self.nodeColumns = kwargs.get('nodeColumns', 16)
-        self.nodeRows = kwargs.get('nodeRows', 5)
-        self.spanColumns = kwargs.get('spanColumns', 8)
-        self.spanRows = kwargs.get('spanRows', 5)
+        self.lineSpacing = kwargs.get('lineSpacing', 2)
         self.shadowOffsetY = kwargs.get('shadowOffsetY', 6)
         self.shadowOffsetX = kwargs.get('shadowOffsetX', 3)
 
-        self.pageMargin = self.cellSize * self.pageMargin
-        self.nodeWidth = self.cellSize * self.nodeColumns
-        self.nodeHeight = self.cellSize * self.nodeRows
-        self.spanWidth = self.cellSize * self.spanColumns
-        self.spanHeight = self.cellSize * self.spanRows
+        self.pageMargin = self.cellSize * kwargs.get('pageMargin', 3)
+        self.nodeWidth = self.cellSize * kwargs.get('nodeColumns', 16)
+        self.nodeHeight = self.cellSize * kwargs.get('nodeRows', 5)
+        self.spanWidth = self.cellSize * kwargs.get('spanColumns', 8)
+        self.spanHeight = self.cellSize * kwargs.get('spanRows', 5)
+
+    def lineSpacing(self):
+        return self.lineSpacing
 
     def node(self, node):
         return NodeMetrix(node, self)
