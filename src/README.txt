846eb02e0fab4b7bb273ceb5eb3424300c01ff5b
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2010-09-29 17:03:37 +0900

    * Revert scale for anti-alias is 2.

---
+++ src/README.txt
@@ -0,0 +1,136 @@
+`blockdiag` generate block-diagram image file from spec-text file.
+
+Features
+========
+
+* Generate block-diagram from dot like text (basic feature).
+* Multilingualization for node-label (utf-8 only).
+
+Setup
+=====
+
+by easy_install
+----------------
+Make environment::
+
+   $ easy_install blockdiag
+
+by buildout
+------------
+Make environment::
+
+   $ hg clone http://bitbucket.org/tk0miya/blockdiag
+   $ cd blockdiag
+   $ python bootstrap.py
+   $ bin/buildout
+
+Copy and modify ini file. example::
+
+   $ cp <blockdiag installed path>/blockdiag/examples/simple.diag .
+   $ vi simple.diag
+
+Please refer to `spec-text setting sample`_ section for the format of the
+`simpla.diag` configuration file.
+
+spec-text setting sample
+========================
+
+Few examples are available.
+
+simple.diag
+------------
+
+simple.diag is simply define nodes and transitions by dot-like text format::
+
+    diagram admin {
+      top_page -> config -> config_edit -> config_confirm -> top_page;
+    }
+
+screen.diag
+------------
+
+screen.diag is more complexly sample. diaglam nodes have a alternative label
+and some transitions::
+
+    diagram admin {
+      top_page [label = "Top page"];
+
+      foo_index [label = "List of FOOs"];
+      foo_detail [label = "Detail FOO"];
+      foo_add [label = "Add FOO"];
+      foo_add_confirm [label = "Add FOO (confirm)"];
+      foo_edit [label = "Edit FOO"];
+      foo_edit_confirm [label = "Edit FOO (confirm)"];
+      foo_delete_confirm [label = "Delete FOO (confirm)"];
+
+      bar_detail [label = "Detail of BAR"];
+      bar_edit [label = "Edit BAR"];
+      bar_edit_confirm [label = "Edit BAR (confirm)"];
+
+      logout;
+
+      top_page -> foo_index;
+      top_page -> bar_detail;
+
+      foo_index -> foo_detail;
+                   foo_detail -> foo_edit;
+                   foo_detail -> foo_delete_confirm;
+      foo_index -> foo_add -> foo_add_confirm -> foo_index;
+      foo_index -> foo_edit -> foo_edit_confirm -> foo_index;
+      foo_index -> foo_delete_confirm -> foo_index;
+
+      bar_detail -> bar_edit -> bar_edit_confirm -> bar_detail;
+    }
+
+
+Usage
+=====
+
+Execute blockdiag command::
+
+   $ blockdiag simple.diag
+   $ ls simple.png
+   simple.png
+
+
+Requirements
+============
+
+* Python 2.4 or later (not support 3.x)
+* Python Imaging Library 1.1.6 or later.
+* funcparserlib 0.3.4 or later.
+* setuptools or distriubte.
+
+
+License
+=======
+Python Software Foundation License.
+
+
+History
+=======
+
+0.3 (2010-09-26)
+------------------
+* Add --antialias option.
+* Fix bugs.
+
+0.2.2 (2010-09-25)
+------------------
+* Fix edge bugs.
+
+0.2.1 (2010-09-25)
+------------------
+* Fix bugs.
+* Fix package style.
+
+0.2 (2010-09-23)
+------------------
+* Update layout engine.
+* Support group { ... } sentence for create Node-Groups.
+* Support numbered badge on node (cf. A [numbered = 5])
+
+0.1 (2010-09-20)
+-----------------
+* first release
+
---
+++ src/blockdiag/blockdiag.py
@@ -0,0 +1,372 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import re
+import uuid
+from optparse import OptionParser
+import DiagramDraw
+import diagparser
+from DiagramMetrix import XY
+
+
+class Screen:
+    def __init__(self, nodes, edges):
+        self.nodes = nodes
+        self.edges = edges
+
+
+class ScreenNode:
+    @classmethod
+    def getId(klass, node):
+        try:
+            node_id = node.id
+        except AttributeError:
+            node_id = node
+
+        return node_id
+
+    def __init__(self, id):
+        self.id = id
+        if id:
+            self.label = re.sub('^"?(.*?)"?$', '\\1', id)
+        else:
+            self.label = ''
+        self.xy = XY(0, 0)
+        self.color = (255, 255, 255)
+        self.group = None
+        self.width = 1
+        self.height = 1
+        self.drawable = 1
+
+    def copyAttributes(self, other):
+        self.label = other.label
+        self.xy = other.xy
+        self.color = other.color
+
+    def setAttributes(self, attrs):
+        for attr in attrs:
+            value = re.sub('^"?(.*?)"?$', '\\1', attr.value)
+            if attr.name == 'label':
+                self.label = value
+            elif attr.name == 'color':
+                self.color = value
+            else:
+                msg = "Unknown node attribute: %s.%s" % (self.id, attr.name)
+                raise AttributeError(msg)
+
+
+class ScreenEdge:
+    def __init__(self, node1, node2):
+        self.node1 = node1
+        self.node2 = node2
+        self.group = None
+        self.color = None
+        self.noweight = None
+        self.skipped = 0
+        self.dir = 'forward'
+
+    def copyAttributes(self, other):
+        self.color = other.color
+        self.noweight = other.noweight
+        self.dir = other.dir
+
+    def setAttributes(self, attrs):
+        for attr in attrs:
+            value = re.sub('^"?(.*?)"?$', '\\1', attr.value)
+            if attr.name == 'color':
+                self.color = value
+            elif attr.name == 'dir':
+                dir = value.lower()
+                if dir in ('back', 'both', 'none'):
+                    self.dir = dir
+                else:
+                    self.dir = 'forward'
+            elif attr.name == 'noweight':
+                if value.lower() == 'none':
+                    self.noweight = None
+                else:
+                    self.noweight = 1
+            else:
+                raise AttributeError("Unknown edge attribute: %s" % attr.name)
+
+
+class ScreenGroup(ScreenNode):
+    def __init__(self, id):
+        ScreenNode.__init__(self, id)
+        self.label = ''
+        self.nodes = []
+        self.edges = []
+        self.color = (243, 152, 0)
+        self.width = 1
+        self.height = 1
+        self.drawable = 0
+
+    def setSize(self, nodes):
+        if len(nodes) > 0:
+            self.width = max(x.xy.x for x in nodes) + 1
+            self.height = max(x.xy.y for x in nodes) + 1
+
+
+class ScreenNodeBuilder:
+    @classmethod
+    def build(klass, tree):
+        return klass()._build(tree)
+
+    def __init__(self):
+        self.uniqNodes = {}
+        self.nodeOrder = []
+        self.uniqLinks = {}
+        self.widthRefs = []
+        self.heightRefs = []
+        self.rows = 0
+
+    def _build(self, tree):
+        self.buildNodeList(tree)
+
+        nodes = self.uniqNodes.values()
+        edges = self.uniqLinks.values()
+        return Screen(nodes, edges)
+
+    def getScreenNode(self, id):
+        if id in self.uniqNodes:
+            node = self.uniqNodes[id]
+        else:
+            node = ScreenNode(id)
+            self.uniqNodes[id] = node
+            self.nodeOrder.append(node)
+
+        return node
+
+    def getScreenGroup(self, id):
+        if id is None:
+            # generate new id
+            id = 'ScreenGroup %s' % uuid.uuid1()
+        else:
+            id = 'ScreenGroup %s' % id
+
+        if id in self.uniqNodes:
+            group = self.uniqNodes[id]
+        else:
+            group = ScreenGroup(id)
+            self.uniqNodes[id] = group
+            self.nodeOrder.append(group)
+
+        return group
+
+    def getScreenEdge(self, id1, id2):
+        link = (self.getScreenNode(id1), self.getScreenNode(id2))
+
+        if link in self.uniqLinks:
+            edge = self.uniqLinks[link]
+        else:
+            edge = ScreenEdge(link[0], link[1])
+            self.uniqLinks[link] = edge
+
+        return edge
+
+    def getChildren(self, node):
+        node_id = ScreenNode.getId(node)
+
+        uniq = {}
+        for edge in self.uniqLinks.values():
+            if edge.noweight == None:
+                if node_id == None:
+                    uniq[edge.node1] = 1
+                elif edge.node1.id == node_id:
+                    uniq[edge.node2] = 1
+                elif edge.node1.group and edge.node1.group.id == node_id:
+                    uniq[edge.node2] = 1
+
+        children = []
+        for node in uniq.keys():
+            if node.group:
+                children.append(node.group)
+            else:
+                children.append(node)
+
+        order = self.nodeOrder
+        children.sort(lambda x, y: cmp(order.index(x), order.index(y)))
+
+        return children
+
+    def isCircularRef(self, node1, node2):
+        node1_id = ScreenNode.getId(node1)
+
+        referenced = False
+        children = [node2]
+        uniqNodes = {}
+        for child in children:
+            if node1_id == child.id:
+                referenced = True
+                break
+
+            for node in self.getChildren(child):
+                if not node in uniqNodes:
+                    children.append(node)
+                    uniqNodes[node] = 1
+
+        return referenced
+
+    def setNodeWidth(self, node=None):
+        node_id = ScreenNode.getId(node)
+
+        self.widthRefs.append(node_id)
+        for child in self.getChildren(node_id):
+            is_ref = child.id in self.widthRefs
+
+            if is_ref and self.isCircularRef(node_id, child):
+                pass
+            elif child.group:
+                pass
+            elif is_ref and node_id == None:
+                pass
+            else:
+                if node_id == None:
+                    child.xy = XY(0, child.xy.y)
+                elif node.xy.x + 1 > child.xy.x:
+                    child.xy = XY(node.xy.x + node.width, child.xy.y)
+                self.setNodeWidth(child)
+
+    def setNodeHeight(self, node, baseHeight):
+        node.xy = XY(node.xy.x, baseHeight)
+        self.heightRefs.append(node.id)
+
+        height = 0
+        for child in self.getChildren(node):
+            if child.id in self.heightRefs:
+                pass
+            elif node.xy.x < child.xy.y:
+                pass
+            else:
+                height += self.setNodeHeight(child, baseHeight + height)
+
+        if height < node.height:
+            height = node.height
+
+        return height
+
+    def buildNodeGroup(self, group, tree):
+        nodes = [x.id for x in tree.stmts if isinstance(x, diagparser.Node)]
+        for edge in self.uniqLinks.values():
+            node1_id = edge.node1.id
+            node2_id = edge.node2.id
+
+            if node1_id in nodes and node2_id in nodes:
+                edge = diagparser.Edge([node1_id, node2_id], [])
+                tree.stmts.append(edge)
+
+        screen = ScreenNodeBuilder.build(tree)
+        if not screen.nodes:
+            return
+
+        group.setSize(screen.nodes)
+
+        for node in screen.nodes:
+            n = self.getScreenNode(node.id)
+            if n.group:
+                msg = "ScreenNode could not belong to two groups"
+                raise RuntimeError(msg)
+            n.copyAttributes(node)
+            n.group = group
+
+            group.nodes.append(n)
+
+        for edge in screen.edges:
+            e = self.getScreenEdge(edge.node1.id, edge.node2.id)
+            e.copyAttributes(edge)
+            e.group = group
+
+            group.edges.append(e)
+
+    def buildNodeList(self, tree):
+        nodeGroups = {}
+        for stmt in tree.stmts:
+            if isinstance(stmt, diagparser.Node):
+                node = self.getScreenNode(stmt.id)
+                node.setAttributes(stmt.attrs)
+            elif isinstance(stmt, diagparser.Edge):
+                while len(stmt.nodes) >= 2:
+                    edge = self.getScreenEdge(stmt.nodes.pop(0), stmt.nodes[0])
+                    edge.setAttributes(stmt.attrs)
+            elif isinstance(stmt, diagparser.SubGraph):
+                group = self.getScreenGroup(stmt.id)
+                nodeGroups[group] = stmt
+            else:
+                raise AttributeError("Unknown sentense: " + str(type(stmt)))
+
+        for group in nodeGroups:
+            self.buildNodeGroup(group, nodeGroups[group])
+
+        self.setNodeWidth()
+
+        height = 0
+        toplevel_nodes = [x for x in self.nodeOrder if x.xy.x == 0]
+        for node in toplevel_nodes:
+            if not node.group:
+                height += self.setNodeHeight(node, height)
+
+        for node in self.nodeOrder:
+            if isinstance(node, ScreenGroup):
+                for child in node.nodes:
+                    child.xy = (node.xy.x + child.xy.x, node.xy.y + child.xy.y)
+
+
+def main():
+    usage = "usage: %prog [options] infile"
+    p = OptionParser(usage=usage)
+    p.add_option('-a', '--antialias', action='store_true',
+                 help='Pass diagram image to anti-alias filter')
+    p.add_option('-o', dest='filename',
+                 help='write diagram to FILE', metavar='FILE')
+    p.add_option('-f', '--font', dest='font',
+                 help='use FONT to draw diagram', metavar='FONT')
+    (options, args) = p.parse_args()
+
+    if len(args) == 0:
+        p.print_help()
+        exit(0)
+
+    if options.antialias:
+        scale = 2
+    else:
+        scale = 1
+
+    fonts = [options.font,
+             'c:/windows/fonts/VL-Gothic-Regular.ttf',
+             'c:/windows/fonts/msmincho.ttf',
+             '/usr/share/fonts/truetype/ipafont/ipagp.ttf',
+             '/System/Library/Fonts/AppleGothic.ttf']
+
+    fontpath = None
+    for path in fonts:
+        if path and os.path.isfile(path):
+            fontpath = path
+            break
+
+    infile = args[0]
+    if options.filename:
+        outfile = options.filename
+    else:
+        outfile = re.sub('\..*', '', infile) + '.png'
+
+    try:
+        tree = diagparser.parse_file(infile)
+        screen = ScreenNodeBuilder.build(tree)
+
+        draw = DiagramDraw.DiagramDraw(scale=scale, font=fontpath)
+        draw.draw(screen)
+    except Exception, e:
+        import traceback
+        traceback.print_exc()
+
+        name = e.__class__.__name__
+        print "[%s] %s" % (name, e)
+        exit(1)
+
+    draw.save(outfile, 'PNG')
+
+
+if __name__ == '__main__':
+    main()
