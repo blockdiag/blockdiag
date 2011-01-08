8161878f4adf77974e157c1245506b69be1e9f89
tk0miya <i.tkomiya@gmail.com>
2011-01-08 16:48:23 +0900

    * Fix DiagramEdge#find_by_level() returned incorrect results

--- src/blockdiag/elements.py
+++ src/blockdiag/elements.py
@@ -266,18 +266,22 @@ class DiagramEdge:
         edges = []
         for e in cls.find_all():
             edge = e.duplicate()
+            skips = 0
 
             if edge.node1.group.level < level:
-                continue
+                skips += 1
             else:
                 while edge.node1.group.level != level:
                     edge.node1 = edge.node1.group
 
             if edge.node2.group.level < level:
-                continue
+                skips += 1
             else:
                 while edge.node2.group.level != level:
                     edge.node2 = edge.node2.group
+
+            if skips == 2:
+                continue
 
             edges.append(edge)
 
