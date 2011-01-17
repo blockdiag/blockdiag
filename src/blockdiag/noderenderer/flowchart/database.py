06531b721a99fc081103b326f46100692a123f5c
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-01-17 18:37:31 +0900

    * Fix pep8 violations in blockdiag.noderenderer.flowcharst.database

--- src/blockdiag/noderenderer/flowchart/database.py
+++ src/blockdiag/noderenderer/flowchart/database.py
@@ -21,28 +21,28 @@ def render_node(drawer, format, node, metrix, **kwargs):
 
     # bottom ellipse
     box = (m.bottomLeft().x, m.bottomLeft().y - r,
-           m.bottomRight().x, m.bottomRight().y);
+           m.bottomRight().x, m.bottomRight().y)
     drawer.ellipse(box, fill=node.color, outline=outline, style=node.style)
     # center rectangle but not outlined
-    box = (m.topLeft().x, m.topLeft().y + (r/2),
-           m.bottomRight().x, m.bottomRight().y - (r/2))
+    box = (m.topLeft().x, m.topLeft().y + (r / 2),
+           m.bottomRight().x, m.bottomRight().y - (r / 2))
     drawer.rectangle(box, fill=node.color, outline=node.color)
     # top ellipse
     box = (m.topLeft().x, m.topLeft().y,
            m.bottomRight().x, m.topRight().y + r)
     drawer.ellipse(box, fill=node.color, outline=outline, style=node.style)
     # line both side
-    line = (XY(m.topLeft().x,m.topLeft().y+(r/2)),
-            XY(m.bottomLeft().x,m.bottomLeft().y-(r/2)))
+    line = (XY(m.topLeft().x, m.topLeft().y + (r / 2)),
+            XY(m.bottomLeft().x, m.bottomLeft().y - (r / 2)))
     drawer.line(line, fill=outline, width=thick, style=node.style)
-    line = (XY(m.topRight().x,m.topRight().y+(r/2)),
-            XY(m.bottomRight().x,m.bottomRight().y-(r/2)))
+    line = (XY(m.topRight().x, m.topRight().y + (r / 2)),
+            XY(m.bottomRight().x, m.bottomRight().y - (r / 2)))
     drawer.line(line, fill=outline, width=thick, style=node.style)
 
     if node.background:
         drawer.loadImage(node.background, m.box())
 
-    box = (m.topLeft().x + r, m.topLeft().y + (r/2),
+    box = (m.topLeft().x + r, m.topLeft().y + (r / 2),
            m.bottomRight().x - r, m.bottomRight().y)
     drawer.textarea(box, node.label, fill=fill,
                     font=font, fontsize=metrix.fontSize,
@@ -57,13 +57,14 @@ def render_node(drawer, format, node, metrix, **kwargs):
         drawer.textarea(box, node.numbered, fill=fill,
                         font=font, fontsize=metrix.fontSize)
 
+
 def render_shadow(drawer, format, node, metrix, fill):
     m = metrix.node(node)
     r = metrix.cellSize * 2
 
     #
     box = (m.bottomLeft().x, m.bottomLeft().y - r,
-           m.bottomRight().x, m.bottomRight().y);
+           m.bottomRight().x, m.bottomRight().y)
     shadow = renderer.shift_box(box, metrix.shadowOffsetX,
                                 metrix.shadowOffsetY)
     drawer.ellipse(shadow, fill=fill, filter='transp-blur')
@@ -74,8 +75,8 @@ def render_shadow(drawer, format, node, metrix, fill):
                                 metrix.shadowOffsetY)
     drawer.ellipse(shadow, fill=fill, filter='transp-blur')
 
-    box = (m.topLeft().x, m.topLeft().y + (r/2),
-           m.bottomRight().x, m.bottomRight().y - (r/2))
+    box = (m.topLeft().x, m.topLeft().y + (r / 2),
+           m.bottomRight().x, m.bottomRight().y - (r / 2))
     shadow = renderer.shift_box(box, metrix.shadowOffsetX,
                                 metrix.shadowOffsetY)
     drawer.rectangle(shadow, fill=fill, filter='transp-blur')
