e92238d77de849e80c44c0301236baed4fe01ec7
tk0miya <i.tkomiya@gmail.com>
2011-02-11 14:27:28 +0900

    * Change to new style noderenderer: endpoint

--- src/blockdiag/noderenderer/endpoint.py
+++ src/blockdiag/noderenderer/endpoint.py
@@ -1,49 +1,39 @@
 # -*- coding: utf-8 -*-
+from blockdiag.noderenderer import NodeShape
 from blockdiag.noderenderer import install_renderer
-from blockdiag.utils import renderer
 from blockdiag.utils.XY import XY
-import blockdiag.DiagramMetrix
 
 
-def render_node(drawer, format, node, metrix, **kwargs):
-    outline = kwargs.get('outline')
-    font = kwargs.get('font')
-    fill = kwargs.get('fill')
-    badgeFill = kwargs.get('badgeFill')
+class EndPoint(NodeShape):
+    def __init__(self, node, metrix=None):
+        super(EndPoint, self).__init__(node, metrix)
 
-    m = metrix.cell(node)
-    r = metrix.cellSize
-    box = (m.top().x - r, m.top().y + metrix.nodeHeight / 2 - r,
-           m.top().x + r, m.top().y + metrix.nodeHeight / 2 + r)
+        self.radius = metrix.cellSize
+        self.center = metrix.cell(node).center()
 
-    drawer.ellipse(box, outline=outline,
-                   fill=node.color, style=node.style)
+    def render_shape(self, drawer, format, **kwargs):
+        outline = kwargs.get('outline')
+        font = kwargs.get('font')
+        fill = kwargs.get('fill')
 
-    textbox = (m.top().x, m.top().y, m.right().x, m.right().y)
-    drawer.textarea(textbox, node.label, fill=fill, halign="left",
-                    font=font, fontsize=metrix.fontSize,
-                    lineSpacing=metrix.lineSpacing)
+        # draw outline
+        r = self.radius
+        box = (self.center.x - r, self.center.y - r,
+               self.center.x + r, self.center.y + r)
+        if kwargs.get('shadow'):
+            box = self.shift_shadow(box)
+            drawer.ellipse(box, fill=fill, outline=fill, filter='transp-blur')
+        else:
+            drawer.ellipse(box, fill=self.node.color, outline=outline,
+                           style=self.node.style)
 
-
-def render_shadow(drawer, format, node, metrix, fill):
-    m = metrix.cell(node)
-    r = metrix.cellSize
-    box = (m.top().x - r, m.top().y + metrix.nodeHeight / 2 - r,
-           m.top().x + r, m.top().y + metrix.nodeHeight / 2 + r)
-
-    shadow = renderer.shift_box(box, metrix.shadowOffsetX,
-                                metrix.shadowOffsetY)
-
-    drawer.ellipse(shadow, fill=fill, filter='transp-blur')
-
-
-class NodeMetrix(object):
-    def __init__(self, node, metrix):
-        self.metrix = metrix
-        top = metrix.cell(node).top()
-
-        self.radius = self.metrix.cellSize
-        self.center = XY(top.x, top.y + metrix.nodeHeight / 2)
+        # draw label
+        if not kwargs.get('shadow'):
+            m = self.metrix.cell(self.node)
+            textbox = (m.top().x, m.top().y, m.right().x, m.right().y)
+            drawer.textarea(textbox, self.node.label, fill=fill, halign="left",
+                            font=font, fontsize=self.metrix.fontSize,
+                            lineSpacing=self.metrix.lineSpacing)
 
     def top(self):
         return XY(self.center.x, self.center.y - self.radius)
@@ -59,4 +49,4 @@ class NodeMetrix(object):
 
 
 def setup(self):
-    install_renderer('endpoint', self)
+    install_renderer('endpoint', EndPoint)
