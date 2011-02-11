8ee428d14e9ff3595f33d3beba2c0c8dcb545b29
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-02-11 14:29:47 +0900

    * Change to new style noderenderer: mail

--- src/blockdiag/noderenderer/mail.py
+++ src/blockdiag/noderenderer/mail.py
@@ -1,52 +1,46 @@
 # -*- coding: utf-8 -*-
+from blockdiag.noderenderer import NodeShape
 from blockdiag.noderenderer import install_renderer
-from blockdiag.utils import renderer
 from blockdiag.utils.XY import XY
 
 
-def render_node(drawer, format, node, metrix, **kwargs):
-    outline = kwargs.get('outline')
-    font = kwargs.get('font')
-    fill = kwargs.get('fill')
-    badgeFill = kwargs.get('badgeFill')
+class Mail(NodeShape):
+    def render_shape(self, drawer, format, **kwargs):
+        outline = kwargs.get('outline')
+        font = kwargs.get('font')
+        fill = kwargs.get('fill')
 
-    m = metrix.cell(node)
-    r = metrix.cellSize * 2
+        m = self.metrix.cell(self.node)
+        r = self.metrix.cellSize * 2
+        textbox = (m.topLeft().x, m.topLeft().y + r,
+                   m.bottomRight().x, m.bottomRight().y)
 
-    box = (m.topLeft().x, m.topLeft().y + r,
-           m.bottomRight().x, m.bottomRight().y)
-    if node.background:
-        drawer.rectangle(m.box(), fill=node.color, outline=node.color)
-        drawer.loadImage(node.background, box)
-        drawer.rectangle(m.box(), outline=outline, style=node.style)
-    else:
-        drawer.rectangle(m.box(), outline=outline,
-                         fill=node.color, style=node.style)
+        # draw outline
+        box = self.metrix.cell(self.node).box()
+        if kwargs.get('shadow'):
+            box = self.shift_shadow(box)
+            drawer.rectangle(box, fill=fill, outline=fill,
+                             filter='transp-blur')
+        elif self.node.background:
+            drawer.rectangle(box, fill=self.node.color,
+                             outline=self.node.color)
+            drawer.loadImage(self.node.background, textbox)
+            drawer.rectangle(box, outline=outline, style=self.node.style)
+        else:
+            drawer.rectangle(box, fill=self.node.color, outline=outline,
+                             style=self.node.style)
 
-    flap = [m.topLeft(), XY(m.top().x, m.top().y + r), m.topRight()]
-    drawer.line(flap, fill=fill, style=node.style)
+        # draw flap
+        if not kwargs.get('shadow'):
+            flap = [m.topLeft(), XY(m.top().x, m.top().y + r), m.topRight()]
+            drawer.line(flap, fill=fill, style=self.node.style)
 
-    drawer.textarea(box, node.label, fill=fill,
-                    font=font, fontsize=metrix.fontSize,
-                    lineSpacing=metrix.lineSpacing)
-
-    if node.numbered != None:
-        xy = m.topLeft()
-        r = metrix.cellSize
-
-        box = (xy.x - r, xy.y - r, xy.x + r, xy.y + r)
-        drawer.ellipse(box, outline=fill, fill=badgeFill)
-        drawer.textarea(box, node.numbered, fill=fill,
-                        font=font, fontsize=metrix.fontSize)
-
-
-def render_shadow(drawer, format, node, metrix, fill):
-    box = metrix.cell(node).box()
-    shadow = renderer.shift_box(box, metrix.shadowOffsetX,
-                                metrix.shadowOffsetY)
-
-    drawer.rectangle(shadow, fill=fill, filter='transp-blur')
+        # draw label
+        if not kwargs.get('shadow'):
+            drawer.textarea(textbox, self.node.label, fill=fill,
+                            font=font, fontsize=self.metrix.fontSize,
+                            lineSpacing=self.metrix.lineSpacing)
 
 
 def setup(self):
-    install_renderer('mail', self)
+    install_renderer('mail', Mail)
