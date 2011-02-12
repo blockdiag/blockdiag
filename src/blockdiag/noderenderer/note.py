a2b0755a2d995032cd7e3bf93ba0cde3df406fbd
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-02-12 15:08:17 +0900

    * Change to new style renderer: note

--- src/blockdiag/noderenderer/note.py
+++ src/blockdiag/noderenderer/note.py
@@ -1,66 +1,51 @@
 # -*- coding: utf-8 -*-
-import math
+from blockdiag.noderenderer import NodeShape
 from blockdiag.noderenderer import install_renderer
 from blockdiag.utils.XY import XY
-from blockdiag.utils import renderer
 
 
-def render_node(drawer, format, node, metrix, **kwargs):
-    outline = kwargs.get('outline')
-    font = kwargs.get('font')
-    fill = kwargs.get('fill')
-    badgeFill = kwargs.get('badgeFill')
+class Note(NodeShape):
+    def render_shape(self, drawer, format, **kwargs):
+        outline = kwargs.get('outline')
+        font = kwargs.get('font')
+        fill = kwargs.get('fill')
 
-    m = metrix.cell(node)
-    w = metrix.cellSize * 2
-    thick = metrix.scale_ratio
+        m = self.metrix.cell(self.node)
+        r = self.metrix.cellSize * 2
 
-    if thick == 1:
-        d = 0
-    else:
-        d = int(math.ceil(thick / 2.0))
+        tr = m.topRight()
+        note = [m.topLeft(), XY(tr.x - r, tr.y), XY(tr.x, tr.y + r),
+                m.bottomRight(), m.bottomLeft(), m.topLeft()]
+        box = self.metrix.cell(self.node).box()
 
-    tr = m.topRight()
-    note = [m.topLeft(), XY(tr.x - w, tr.y), XY(tr.x, tr.y + w),
-            m.bottomRight(), m.bottomLeft(), m.topLeft()]
+        # draw outline
+        if kwargs.get('shadow'):
+            note = self.shift_shadow(note)
+            drawer.polygon(note, fill=fill, outline=fill,
+                           filter='transp-blur')
+        elif self.node.background:
+            drawer.polygon(note, fill=self.node.color,
+                             outline=self.node.color)
+            drawer.loadImage(self.node.background, box)
+            drawer.polygon(note, fill="none", outline=outline,
+                           style=self.node.style)
+        else:
+            drawer.polygon(note, fill=self.node.color, outline=outline,
+                           style=self.node.style)
 
-    if node.background:
-        drawer.polygon(note, fill=node.color)
-        drawer.loadImage(node.background, m.box())
-        drawer.polygon(note, fill="none", outline=outline,
-                       style=node.style)
-    else:
-        drawer.polygon(note, fill=node.color, outline=outline)
+        # draw folded
+        if not kwargs.get('shadow'):
+            folded = [XY(tr.x - r, tr.y),
+                      XY(tr.x - r, tr.y + r),
+                      XY(tr.x, tr.y + r)]
+            drawer.line(folded, fill=fill, style=self.node.style)
 
-    folded = [XY(tr.x - w, tr.y), XY(tr.x - w, tr.y + w),
-              XY(tr.x, tr.y + w)]
-    drawer.line(folded, fill=fill, style=node.style)
-
-    drawer.textarea(m.box(), node.label, fill=fill,
-                    font=font, fontsize=metrix.fontSize,
-                    lineSpacing=metrix.lineSpacing)
-
-    if node.numbered != None:
-        xy = m.topLeft()
-        r = metrix.cellSize
-
-        box = (xy.x - r, xy.y - r, xy.x + r, xy.y + r)
-        drawer.ellipse(box, outline=fill, fill=badgeFill)
-        drawer.textarea(box, node.numbered, fill=fill,
-                        font=font, fontsize=metrix.fontSize)
-
-
-def render_shadow(drawer, format, node, metrix, fill):
-    m = metrix.cell(node)
-    w = metrix.cellSize
-
-    tr = m.topRight()
-    note = [m.topLeft(), XY(tr.x - w, tr.y), XY(tr.x, tr.y + w),
-            m.bottomRight(), m.bottomLeft(), m.topLeft()]
-    shadow = renderer.shift_polygon(note, metrix.shadowOffsetX,
-                                    metrix.shadowOffsetY)
-    drawer.polygon(shadow, fill=fill, filter='transp-blur')
+        # draw label
+        if not kwargs.get('shadow'):
+            drawer.textarea(box, self.node.label, fill=fill,
+                            font=font, fontsize=self.metrix.fontSize,
+                            lineSpacing=self.metrix.lineSpacing)
 
 
 def setup(self):
-    install_renderer('note', self)
+    install_renderer('note', Note)
