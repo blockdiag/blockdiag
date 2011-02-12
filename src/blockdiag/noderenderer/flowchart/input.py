a8360f791fd3055b1fcb677059d1c7c74706b594
tk0miya <i.tkomiya@gmail.com>
2011-02-12 16:05:23 +0900

    * Change to new style renderer: flowchart.input

--- src/blockdiag/noderenderer/flowchart/input.py
+++ src/blockdiag/noderenderer/flowchart/input.py
@@ -1,68 +1,47 @@
 # -*- coding: utf-8 -*-
-import math
+from blockdiag.noderenderer import NodeShape
 from blockdiag.noderenderer import install_renderer
 from blockdiag.utils.XY import XY
-from blockdiag.utils import renderer
 
 
-def render_node(drawer, format, node, metrix, **kwargs):
-    outline = kwargs.get('outline')
-    font = kwargs.get('font')
-    fill = kwargs.get('fill')
-    badgeFill = kwargs.get('badgeFill')
+class Input(NodeShape):
+    def render_shape(self, drawer, format, **kwargs):
+        outline = kwargs.get('outline')
+        font = kwargs.get('font')
+        fill = kwargs.get('fill')
 
-    m = metrix.cell(node)
-    w = metrix.cellSize * 2
-    thick = metrix.scale_ratio
+        m = self.metrix.cell(self.node)
+        r = self.metrix.cellSize * 2
 
-    if thick == 1:
-        d = 0
-    else:
-        d = int(math.ceil(thick / 2.0))
+        shape = [XY(m.topLeft().x + r,  m.topLeft().y),
+                 XY(m.topRight().x + r, m.topRight().y),
+                 XY(m.bottomRight().x - r, m.bottomRight().y),
+                 XY(m.bottomLeft().x - r,  m.bottomLeft().y),
+                 XY(m.topLeft().x + r,  m.topLeft().y)]
+        textbox = (m.topLeft().x + r, m.topLeft().y,
+                   m.bottomRight().x - r, m.bottomRight().y)
 
-    parallel = [XY(m.topLeft().x + w,  m.topLeft().y),
-                XY(m.topRight().x + w, m.topRight().y),
-                XY(m.bottomRight().x - w, m.bottomRight().y),
-                XY(m.bottomLeft().x - w,  m.bottomLeft().y),
-                XY(m.topLeft().x + w,  m.topLeft().y)]
+        # draw outline
+        if kwargs.get('shadow'):
+            shape = self.shift_shadow(shape)
+            drawer.polygon(shape, fill=fill, outline=fill,
+                           filter='transp-blur')
+        elif self.node.background:
+            drawer.polygon(shape, fill=self.node.color,
+                             outline=self.node.color)
+            drawer.loadImage(self.node.background, textbox)
+            drawer.polygon(shape, fill="none", outline=outline,
+                           style=self.node.style)
+        else:
+            drawer.polygon(shape, fill=self.node.color, outline=outline,
+                           style=self.node.style)
 
-    box = (m.topLeft().x + w, m.topLeft().y,
-           m.bottomRight().x - w, m.bottomRight().y)
-    if node.background:
-        drawer.polygon(parallel, fill=node.color)
-        drawer.loadImage(node.background, box)
-        drawer.polygon(parallel, fill="none", outline=outline,
-                       style=node.style)
-    else:
-        drawer.polygon(parallel, fill=node.color, outline=outline)
-
-    drawer.textarea(box, node.label, fill=fill,
-                    font=font, fontsize=metrix.fontSize,
-                    lineSpacing=metrix.lineSpacing)
-
-    if node.numbered != None:
-        xy = m.topLeft()
-        r = metrix.cellSize
-
-        box = (xy.x - r, xy.y - r, xy.x + r, xy.y + r)
-        drawer.ellipse(box, outline=fill, fill=badgeFill)
-        drawer.textarea(box, node.numbered, fill=fill,
-                        font=font, fontsize=metrix.fontSize)
-
-
-def render_shadow(drawer, format, node, metrix, fill):
-    m = metrix.cell(node)
-    w = metrix.cellSize * 2
-
-    parallel = [(m.topLeft().x + w,  m.topLeft().y),
-                (m.topRight().x + w, m.topRight().y),
-                (m.bottomRight().x - w, m.bottomRight().y),
-                (m.bottomLeft().x - w,  m.bottomLeft().y),
-                (m.topLeft().x + w,  m.topLeft().y)]
-    shadow = renderer.shift_polygon(parallel, metrix.shadowOffsetX,
-                                    metrix.shadowOffsetY)
-    drawer.polygon(shadow, fill=fill, filter='transp-blur')
+        # draw label
+        if not kwargs.get('shadow'):
+            drawer.textarea(textbox, self.node.label, fill=fill,
+                            font=font, fontsize=self.metrix.fontSize,
+                            lineSpacing=self.metrix.lineSpacing)
 
 
 def setup(self):
-    install_renderer('flowchart.input', self)
+    install_renderer('flowchart.input', Input)
