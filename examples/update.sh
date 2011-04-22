3c02a73f62366f5d014681ad5d6c1a1a7c6aa59b
tk0miya <i.tkomiya@gmail.com>
2011-04-22 18:06:17 +0900

    * Regenerate images

--- examples/update.sh
+++ examples/update.sh
@@ -8,4 +8,8 @@ do
     png=$EXAMPLES/`basename $diag .diag`.png
     echo $BLOCKDIAG -Tpng -o $png $diag
     $BLOCKDIAG -Tpng -o $png $diag
+
+    svg=$EXAMPLES/`basename $diag .diag`.svg
+    echo $BLOCKDIAG -Tsvg -o $svg $diag
+    $BLOCKDIAG -Tsvg -o $svg $diag
 done
