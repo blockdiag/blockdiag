5a9e666780b9080a073b78d4d1f860a12e6f7dc3
tk0miya <i.tkomiya@gmail.com>
2011-04-28 00:34:03 +0900

    * Refactored; DiagramEdge.find() supports NodeGroup filter

--- src/blockdiag/elements.py
+++ src/blockdiag/elements.py
@@ -278,11 +278,18 @@ class DiagramEdge(Base):
 
     @classmethod
     def find(cls, node1, node2=None):
-        if node1 is None:
-            return [e for e in cls.find_all() if e.node2 == node2]
+        if node1 is None and node2 is None:
+            return cls.find_all()
         elif isinstance(node1, NodeGroup):
             edges = cls.find(None, node2)
-            return [e for e in edges if e.node1.group.is_parent(node1)]
+            edges = (e for e in edges if e.node1.group.is_parent(node1))
+            return [e for e in edges if not e.node2.group.is_parent(node1)]
+        elif isinstance(node2, NodeGroup):
+            edges = cls.find(node1, None)
+            edges = (e for e in edges if e.node2.group.is_parent(node2))
+            return [e for e in edges if not e.node1.group.is_parent(node2)]
+        elif node1 is None:
+            return [e for e in cls.find_all() if e.node2 == node2]
         else:
             if node1 not in cls.namespace:
                 return []
