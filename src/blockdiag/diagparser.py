2eed8cd3dd516d40aed960fa3567c9d05324fb5c
tk0miya <i.tkomiya@gmail.com>
2011-05-14 13:53:34 +0900

    * Wrap parsing errors with diagparser.ParseException

--- src/blockdiag/diagparser.py
+++ src/blockdiag/diagparser.py
@@ -57,6 +57,10 @@ Edge = namedtuple('Edge', 'nodes attrs')
 DefAttrs = namedtuple('DefAttrs', 'object attrs')
 
 
+class ParseException(Exception):
+    pass
+
+
 def tokenize(str):
     'str -> Sequence(Token)'
     specs = [
@@ -145,5 +149,11 @@ def parse(seq):
 
 
 def parse_file(path):
-    input = codecs.open(path, 'r', 'utf-8').read()
-    return parse(tokenize(input))
+    try:
+        input = codecs.open(path, 'r', 'utf-8').read()
+        return parse(tokenize(input))
+    except LexerError, e:
+        message = "Got unexpected token at line %d column %d" % e.place
+        raise ParseException, message
+    except Exception, e:
+        raise ParseException, str(e)
