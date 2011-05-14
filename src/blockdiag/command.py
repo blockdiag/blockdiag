e3a31bcce369509a20bd447f738b316a4886270b
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-05-14 12:30:27 +0900

    * Catch parsing exceptions

--- src/blockdiag/command.py
+++ src/blockdiag/command.py
@@ -122,7 +122,12 @@ def main():
 
     fontpath = detectfont(options)
 
-    tree = diagparser.parse_file(infile)
+    try:
+        tree = diagparser.parse_file(infile)
+    except Exception, e:
+        sys.stderr.write("ERROR: %s\n" % e)
+        return
+
     if options.separate:
         from builder import SeparateDiagramBuilder
 
