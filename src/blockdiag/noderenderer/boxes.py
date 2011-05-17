3a97dc78699d462a1dec3bc8998a37a9f378650a
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-05-17 17:23:15 +0900

    * Reimplement shape; boxes

--- src/blockdiag/noderenderer/boxes.py
+++ src/blockdiag/noderenderer/boxes.py
@@ -13,54 +13,44 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from blockdiag.noderenderer import NodeShape
+from blockdiag.noderenderer.box import Box
 from blockdiag.noderenderer import install_renderer
 from blockdiag.utils.XY import XY
 
 
-class Boxes(NodeShape):
+class Boxes(Box):
     def __init__(self, node, metrix=None):
         super(Boxes, self).__init__(node, metrix)
 
         self.boxes = 3
-        self.r = self.metrix.nodeHeight / 10
+        self.r = self.metrix.cellSize / 2
 
-        box = self.textbox
-        r = self.boxes * self.r
-        self.textbox = (box[0] + r, box[1] + r, box[2], box[3])
+        r = (self.boxes - 1) * self.r
+        self.connectors[1] = XY(self.connectors[1].x + r, self.connectors[1].y)
+        self.connectors[2] = XY(self.connectors[2].x, self.connectors[2].y + r)
 
     def render_shape(self, drawer, format, **kwargs):
         outline = kwargs.get('outline')
-        drawer = drawer
         fill = kwargs.get('fill')
 
         # draw outline
-        box = self.metrix.cell(self.node).box()
         if kwargs.get('shadow'):
-            for i in range(self.boxes - 1, -1, -1):
+            for i in range(self.boxes - 1, 0, -1):
                 r = i * self.r
-                box = (self.textbox[0] - r, self.textbox[1] - r,
-                       self.textbox[2] - r, self.textbox[3] - r)
+                box = (self.textbox[0] + r, self.textbox[1] + r,
+                       self.textbox[2] + r, self.textbox[3] + r)
                 shadow = self.shift_shadow(box)
                 drawer.rectangle(shadow, fill=fill, outline=fill,
                                  filter='transp-blur')
         else:
             for i in range(self.boxes - 1, 0, -1):
                 r = i * self.r
-                box = (self.textbox[0] - r, self.textbox[1] - r,
-                       self.textbox[2] - r, self.textbox[3] - r)
+                box = (self.textbox[0] + r, self.textbox[1] + r,
+                       self.textbox[2] + r, self.textbox[3] + r)
                 drawer.rectangle(box, fill=self.node.color, outline=outline,
                                  style=self.node.style)
 
-            box = self.textbox
-            if self.node.background:
-                drawer.rectangle(box, fill=self.node.color,
-                                 outline=self.node.color)
-                drawer.loadImage(self.node.background, self.textbox)
-                drawer.rectangle(box, outline=outline, style=self.node.style)
-            else:
-                drawer.rectangle(box, fill=self.node.color, outline=outline,
-                                 style=self.node.style)
+        super(Boxes, self).render_shape(drawer, format, **kwargs)
 
 
 def setup(self):
