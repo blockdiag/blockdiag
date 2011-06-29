d7c74b3bdd18ce4908d3bfb091941c8877f79413
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-06-29 20:48:58 +0900

    * Fix loading blockdiag.utils.ellipse on demand

--- src/blockdiag/imagedraw/svg.py
+++ src/blockdiag/imagedraw/svg.py
@@ -16,7 +16,6 @@
 import re
 import sys
 import base64
-from blockdiag import utils
 from blockdiag.utils.XY import XY
 from blockdiag.utils import urlutil
 from SVGdraw import *
@@ -155,12 +154,14 @@ class SVGImageDrawElement:
         if start > end:
             end += 360
 
-        coord = utils.ellipse.coordinate(1, w, h, start, start + 1)
+        from blockdiag.utils import ellipse
+
+        coord = ellipse.coordinate(1, w, h, start, start + 1)
         point = iter(coord).next()
         pt1 = XY(xy[0] + w + round(point[0], 0),
                  xy[1] + h + round(point[1], 0))
 
-        coord = utils.ellipse.coordinate(1, w, h, end, end + 1)
+        coord = ellipse.coordinate(1, w, h, end, end + 1)
         point = iter(coord).next()
         pt2 = XY(xy[0] + w + round(point[0], 0),
                  xy[1] + h + round(point[1], 0))
