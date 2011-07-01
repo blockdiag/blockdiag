66d762a2cd5cf7f39b0836b8b7d7947e3f5a53e0
tk0miya <i.tkomiya@gmail.com>
2011-07-01 21:24:07 +0900

    * Support input from stdin

--- src/blockdiag/diagparser.py
+++ src/blockdiag/diagparser.py
@@ -148,12 +148,16 @@ def parse(seq):
     return dotfile.parse(seq)
 
 
-def parse_file(path):
+def parse_string(string):
     try:
-        input = codecs.open(path, 'r', 'utf-8').read()
-        return parse(tokenize(input))
+        return parse(tokenize(string))
     except LexerError, e:
         message = "Got unexpected token at line %d column %d" % e.place
         raise ParseException(message)
     except Exception, e:
         raise ParseException(str(e))
+
+
+def parse_file(path):
+    input = codecs.open(path, 'r', 'utf-8').read()
+    return parse_string(input)
