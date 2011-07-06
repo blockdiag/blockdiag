09ce08e9011cbb7af114fdaa406937b168fc742b
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-07-06 21:03:31 +0900

    * Move blockdiag.LineJumpDrawFilter to blockdiag.imagedraw.filters

--- src/blockdiag/imagedraw/filters/linejump.py
+++ src/blockdiag/imagedraw/filters/linejump.py
@@ -13,7 +13,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from utils.XY import XY
+from blockdiag.utils.XY import XY
 
 
 class LazyReciever(object):
