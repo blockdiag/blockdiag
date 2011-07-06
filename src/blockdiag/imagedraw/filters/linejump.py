4c7e3b9938b0d4d2960b962abb33049efe8dd248
tk0miya <i.tkomiya@gmail.com>
2011-07-06 21:24:49 +0900

    * Fix pep8 violation

--- src/blockdiag/imagedraw/filters/linejump.py
+++ src/blockdiag/imagedraw/filters/linejump.py
@@ -66,7 +66,8 @@ class LineJumpDrawFilter(LazyReciever):
                     for x in sorted(self.cross.get(y, [])):
                         if x1 <= x and x <= x2:
                             r = self.jump_radius
-                            self.target.line((XY(x1, y), XY(x - r, y)), **kwargs)
+                            line = (XY(x1, y), XY(x - r, y))
+                            self.target.line(line, **kwargs)
                             box = (x - r, y - r, x + r, y + r)
                             self.target.arc(box, 180, 0, **kwargs)
                             x1 = x + r
