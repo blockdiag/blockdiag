58116e679a83aa3b7b37e6c16f87e03c6ce2ce05
tk0miya <i.tkomiya@gmail.com>
2011-07-29 11:38:22 +0900

    * Fix failure with background attribute in python2.4

--- src/blockdiag/utils/urlutil.py
+++ src/blockdiag/utils/urlutil.py
@@ -6,7 +6,7 @@ import urlparse
 def isurl(url):
     o = urlparse.urlparse(url)
     accpetable = ["http", "https"]
-    if o.scheme in accpetable:
+    if o[0] in accpetable:
         return True
     else:
         return False
