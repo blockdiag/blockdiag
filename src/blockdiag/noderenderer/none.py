40530d9c7d8f77d0a8c16d198cd0a54288e20228
tk0miya <i.tkomiya@gmail.com>
2011-09-02 10:32:17 +0900

    * Centerize edge connectors on NoneShape

--- src/blockdiag/noderenderer/none.py
+++ src/blockdiag/noderenderer/none.py
@@ -4,6 +4,12 @@ from blockdiag.noderenderer import install_renderer
 
 
 class NoneShape(NodeShape):
+    def __init__(self, node, metrix=None):
+        super(NoneShape, self).__init__(node, metrix)
+
+        p = metrix.cell(node).center()
+        self.connectors = [p, p, p, p]
+
     def render_label(self, drawer, **kwargs):
         pass
 
