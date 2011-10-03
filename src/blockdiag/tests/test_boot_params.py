8af93e22a114834dd12a06d58f924844197842b8
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-10-03 21:15:37 +0900

    * Fix testcode

--- src/blockdiag/tests/test_boot_params.py
+++ src/blockdiag/tests/test_boot_params.py
@@ -4,7 +4,7 @@ import os
 import sys
 import tempfile
 from nose.tools import raises
-from blockdiag.command import parse_option, main
+from blockdiag.command import parse_option, detectfont
 
 
 def replace_argv(func):
@@ -40,54 +40,54 @@ def type_option_test():
 @replace_argv
 def invalid_type_option_test():
     sys.argv = ['', '-Tsvgz', 'input.diag']
-    option = parse_option()
+    (options, args) = parse_option()
 
 
 @replace_argv
 def svg_separate_option_test():
     sys.argv = ['', '-Tsvg', '--separate', 'input.diag']
-    option = parse_option()
+    (options, args) = parse_option()
 
 
 @raises(RuntimeError)
 @replace_argv
 def png_separate_option_test():
     sys.argv = ['', '-Tpng', '--separate', 'input.diag']
-    option = parse_option()
+    (options, args) = parse_option()
 
 
 @raises(RuntimeError)
 @replace_argv
 def pdf_separate_option_test():
     sys.argv = ['', '-Tpdf', '--separate', 'input.diag']
-    option = parse_option()
+    (options, args) = parse_option()
 
 
 @replace_argv
 def svg_nodoctype_option_test():
     sys.argv = ['', '-Tsvg', '--nodoctype', 'input.diag']
-    option = parse_option()
+    (options, args) = parse_option()
 
 
 @raises(RuntimeError)
 @replace_argv
 def png_nodoctype_option_test():
     sys.argv = ['', '-Tpng', '--nodoctype', 'input.diag']
-    option = parse_option()
+    (options, args) = parse_option()
 
 
 @raises(RuntimeError)
 @replace_argv
 def pdf_nodoctype_option_test():
     sys.argv = ['', '-Tpdf', '--nodoctype', 'input.diag']
-    option = parse_option()
+    (options, args) = parse_option()
 
 
 @replace_argv
 def config_option_test():
     tmp = tempfile.mkstemp()
     sys.argv = ['', '-c', tmp[1], 'input.diag']
-    option = parse_option()
+    (options, args) = parse_option()
 
     os.close(tmp[0])
     os.unlink(tmp[1])
@@ -101,7 +101,7 @@ def invalid_config_option_test():
     os.unlink(tmp[1])
 
     sys.argv = ['', '-c', tmp[1], 'input.diag']
-    option = parse_option()
+    (options, args) = parse_option()
 
 
 @raises(RuntimeError)
@@ -111,7 +111,7 @@ def invalid_dir_config_option_test():
         tmp = tempfile.mkdtemp()
 
         sys.argv = ['', '-c', tmp, 'input.diag']
-        option = parse_option()
+        (options, args) = parse_option()
     finally:
         os.rmdir(tmp)
 
@@ -123,6 +123,6 @@ def not_exist_font_config_option_test():
         tmp = tempfile.mkdtemp()
 
         sys.argv = ['', '-f', '/font_is_not_exist', '-c', tmp, 'input.diag']
-        option = parse_option()
+        (options, args) = parse_option()
     finally:
         os.rmdir(tmp)
