362bab56d356950504d0f368ed1208148a952951
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-10-03 12:59:39 +0900

    * Replace assert() by nose.tools

--- src/blockdiag/tests/test_builder_separate.py
+++ src/blockdiag/tests/test_builder_separate.py
@@ -17,14 +17,14 @@ def __build_diagram(filename):
 
 
 def test_separate1_diagram():
-    screen = __build_diagram('separate1.diag')
+    diagram = __build_diagram('separate1.diag')
 
     assert_pos = {0: {'B': (0, 0), 'C': (1, 0), 'D': (4, 0),
                       'E': (2, 0), 'F': (3, 0)},
                   1: {'A': (0, 0), 'B': (1, 0), 'D': (3, 0)},
                   2: {'A': (0, 0), 'Z': (0, 1)}}
 
-    for i, diagram in enumerate(screen):
+    for i, diagram in enumerate(diagram):
         for node in diagram.traverse_nodes():
             if isinstance(node, DiagramNode):
                 print node, assert_pos[i][node.id]
@@ -32,7 +32,7 @@ def test_separate1_diagram():
 
 
 def test_separate2_diagram():
-    screen = __build_diagram('separate2.diag')
+    diagram = __build_diagram('separate2.diag')
 
     assert_pos = {0: {'A': (0, 0), 'C': (1, 0), 'D': (2, 0),
                       'E': (0, 2), 'G': (3, 0), 'H': (3, 1)},
@@ -41,7 +41,7 @@ def test_separate2_diagram():
                   2: {'A': (0, 0), 'F': (2, 2), 'G': (2, 0),
                       'H': (2, 1), 'Z': (0, 3)}}
 
-    for i, diagram in enumerate(screen):
+    for i, diagram in enumerate(diagram):
         for node in diagram.traverse_nodes():
             if isinstance(node, DiagramNode):
                 print node, assert_pos[i][node.id]
