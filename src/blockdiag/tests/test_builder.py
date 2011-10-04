90f6a736bd1e553bff3476f26c6e79f56f8009fd
tk0miya <i.tkomiya@gmail.com>
2011-10-04 17:02:54 +0900

    * Move common test utility functions to blockdiag.tests.utils

--- src/blockdiag/tests/test_builder.py
+++ src/blockdiag/tests/test_builder.py
@@ -1,31 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import tempfile
 from nose.tools import eq_
-from blockdiag.builder import *
-from blockdiag.elements import *
-from blockdiag.diagparser import parse_string
-
-
-def __build_diagram(filename):
-    import os
-    testdir = os.path.dirname(__file__)
-    pathname = "%s/diagrams/%s" % (testdir, filename)
-
-    str = open(pathname).read()
-    tree = parse_string(str)
-    return ScreenNodeBuilder.build(tree)
-
-
-def __validate_node_attributes(filename, **kwargs):
-    diagram = __build_diagram(filename)
-
-    for name, values in kwargs.items():
-        print "[node.%s]" % name
-        for node in (n for n in diagram.nodes  if n.drawable):
-            print node
-            value = getattr(node, name)
-            eq_(values[node.id], value)
+from utils import __build_diagram, __validate_node_attributes
 
 
 def test_diagram_attributes():
