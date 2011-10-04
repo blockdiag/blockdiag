676e44141041429434811a3832a35926ad019529
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-10-04 18:53:08 +0900

    * Separate test_builder.py

---
+++ src/blockdiag/tests/test_bulider_group.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+
+from nose.tools import eq_
+from utils import __build_diagram, __validate_node_attributes
+
+
+def test_nested_groups_diagram():
+    positions = {'A': (0, 0), 'B': (0, 1), 'Z': (0, 2)}
+    __validate_node_attributes('nested_groups.diag', xy=positions)
+
+
+def test_nested_groups_and_edges_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0), 'Z': (0, 1)}
+    __validate_node_attributes('nested_groups_and_edges.diag', xy=positions)
+
+
+def test_empty_group_diagram():
+    positions = {'Z': (0, 0)}
+    __validate_node_attributes('empty_group.diag', xy=positions)
+
+
+def test_empty_nested_group_diagram():
+    positions = {'Z': (0, 0)}
+    __validate_node_attributes('empty_nested_group.diag', xy=positions)
+
+
+def test_empty_group_declaration_diagram():
+    positions = {'A': (0, 0), 'Z': (0, 1)}
+    __validate_node_attributes('empty_group_declaration.diag', xy=positions)
+
+
+def test_simple_group_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1), 'Z': (0, 2)}
+    __validate_node_attributes('simple_group.diag', xy=positions)
+
+
+def test_group_declare_as_node_attribute_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0),
+                  'D': (2, 1), 'E': (2, 2), 'Z': (0, 3)}
+    __validate_node_attributes('group_declare_as_node_attribute.diag',
+                               xy=positions)
+
+
+def test_group_attribute():
+    diagram = __build_diagram('group_attribute.diag')
+
+    for node in (x for x in diagram.nodes if not x.drawable):
+        node.color = 'red'
+        node.label = 'group label'
+
+
+def test_merge_groups_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (0, 1),
+                 'D': (1, 1), 'Z': (0, 2)}
+    __validate_node_attributes('merge_groups.diag', xy=positions)
+
+
+def test_node_attribute_and_group_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0), 'Z': (0, 1)}
+    labels = {'A': 'foo', 'B': 'bar', 'C': 'baz', 'Z': 'Z'}
+    colors = {'A': (255, 0, 0), 'B': '#888888', 'C': (0, 0, 255),
+              'Z': (255, 255, 255)}
+    __validate_node_attributes('node_attribute_and_group.diag',
+                               xy=positions, label=labels, color=colors)
+
+
+def test_group_sibling_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 2), 'D': (2, 0),
+                 'E': (2, 1), 'F': (2, 2), 'Z': (0, 3)}
+    __validate_node_attributes('group_sibling.diag', xy=positions)
+
+
+def test_group_order_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1), 'Z': (0, 2)}
+    __validate_node_attributes('group_order.diag', xy=positions)
+
+
+def test_group_order2_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1),
+                 'D': (2, 1), 'E': (1, 2), 'F': (2, 2), 'Z': (0, 3)}
+    __validate_node_attributes('group_order2.diag', xy=positions)
+
+
+def test_group_order3_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0),
+                 'D': (2, 1), 'E': (1, 2), 'Z': (0, 3)}
+    __validate_node_attributes('group_order3.diag', xy=positions)
+
+
+def test_group_children_height_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1), 'D': (1, 2),
+                 'E': (2, 0), 'F': (2, 2), 'Z': (0, 3)}
+    __validate_node_attributes('group_children_height.diag', xy=positions)
+
+
+def test_group_children_order_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1), 'D': (1, 2),
+                 'E': (2, 0), 'F': (2, 1), 'G': (2, 2), 'Z': (0, 3)}
+    __validate_node_attributes('group_children_order.diag', xy=positions)
+
+
+def test_group_children_order2_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1), 'D': (1, 2),
+                 'E': (2, 1), 'F': (2, 0), 'G': (2, 2), 'Z': (0, 3)}
+    __validate_node_attributes('group_children_order2.diag', xy=positions)
+
+
+def test_group_children_order3_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1), 'D': (1, 2),
+                 'E': (2, 0), 'F': (2, 1), 'G': (2, 2), 'Q': (0, 3),
+                 'Z': (0, 4)}
+    __validate_node_attributes('group_children_order3.diag', xy=positions)
+
+
+def test_node_in_group_follows_outer_node_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0), 'Z': (0, 1)}
+    __validate_node_attributes('node_in_group_follows_outer_node.diag',
+                               xy=positions)
+
+
+def test_group_id_and_node_id_are_not_conflicted_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (0, 1),
+                 'D': (1, 1), 'Z': (0, 2)}
+    __validate_node_attributes('group_id_and_node_id_are_not_conflicted.diag',
+                               xy=positions)
+
+
+def test_outer_node_follows_node_in_group_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0), 'Z': (0, 1)}
+    __validate_node_attributes('outer_node_follows_node_in_group.diag',
+                               xy=positions)
+
+
+def test_large_group_and_node_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1), 'D': (1, 2),
+                 'E': (1, 3), 'F': (2, 0), 'Z': (0, 4)}
+    __validate_node_attributes('large_group_and_node.diag', xy=positions)
+
+
+def test_large_group_and_node2_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0), 'D': (3, 0),
+                 'E': (4, 0), 'F': (5, 0), 'Z': (0, 1)}
+    __validate_node_attributes('large_group_and_node2.diag', xy=positions)
+
+
+def test_large_group_and_two_nodes_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1), 'D': (1, 2),
+                 'E': (1, 3), 'F': (2, 0), 'G': (2, 1), 'Z': (0, 4)}
+    __validate_node_attributes('large_group_and_two_nodes.diag', xy=positions)
+
+
+def test_group_height_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0),
+                 'D': (2, 1), 'E': (1, 2), 'Z': (0, 3)}
+    __validate_node_attributes('group_height.diag', xy=positions)
+
+
+def test_multiple_groups_diagram():
+    positions = {'A': (0, 0), 'B': (0, 1), 'C': (0, 2), 'D': (0, 3),
+                 'E': (1, 0), 'F': (1, 1), 'G': (1, 2), 'H': (2, 0),
+                 'I': (2, 1), 'J': (3, 0), 'Z': (0, 4)}
+    __validate_node_attributes('multiple_groups.diag', xy=positions)
+
+
+def test_multiple_nested_groups_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1), 'Z': (0, 2)}
+    __validate_node_attributes('multiple_nested_groups.diag', xy=positions)
+
+
+def test_group_works_node_decorator_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (3, 0),
+                 'D': (2, 0), 'E': (1, 1), 'Z': (0, 2)}
+    __validate_node_attributes('group_works_node_decorator.diag', xy=positions)
+
+
+def test_nested_groups_work_node_decorator_diagram():
+    positions = {'A': (0, 0), 'B': (0, 1), 'Z': (0, 2)}
+    __validate_node_attributes('nested_groups_work_node_decorator.diag',
+                               xy=positions)
+
+
+def test_reversed_multiple_groups_diagram():
+    positions = {'A': (3, 0), 'B': (3, 1), 'C': (3, 2), 'D': (3, 3),
+                 'E': (2, 0), 'F': (2, 1), 'G': (2, 2), 'H': (1, 0),
+                 'I': (1, 1), 'J': (0, 0), 'Z': (0, 4)}
+    __validate_node_attributes('reverse_multiple_groups.diag', xy=positions)
+
+
+def test_group_and_skipped_edge_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0),
+                 'D': (3, 0), 'E': (1, 1), 'Z': (0, 2)}
+    __validate_node_attributes('group_and_skipped_edge.diag', xy=positions)
+
+
+def test_group_orientation_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1),
+                 'D': (2, 1), 'Z': (0, 2)}
+    __validate_node_attributes('group_orientation.diag', xy=positions)
+
+
+def test_nested_group_orientation_diagram():
+    positions = {'A': (0, 0), 'B': (0, 1), 'C': (1, 0), 'Z': (0, 2)}
+    __validate_node_attributes('nested_group_orientation.diag', xy=positions)
---
+++ src/blockdiag/tests/test_bulider_node.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+from nose.tools import eq_
+from utils import __build_diagram, __validate_node_attributes
+
+
+def test_single_node_diagram():
+    diagram = __build_diagram('single_node.diag')
+
+    assert len(diagram.nodes) == 1
+    assert len(diagram.edges) == 0
+    assert diagram.nodes[0].label == 'A'
+    assert diagram.nodes[0].xy == (0, 0)
+
+
+def test_node_shape_diagram():
+    shapes = {'A': 'box', 'B': 'roundedbox', 'C': 'diamond',
+              'D': 'ellipse', 'E': 'note', 'F': 'cloud',
+              'G': 'mail', 'H': 'beginpoint', 'I': 'endpoint',
+              'J': 'minidiamond', 'K': 'flowchart.condition',
+              'L': 'flowchart.database', 'M': 'flowchart.input',
+              'N': 'flowchart.loopin', 'O': 'flowchart.loopout',
+              'P': 'actor', 'Q': 'flowchart.terminator', 'R': 'textbox',
+              'S': 'dots', 'T': 'none', 'Z': 'box'}
+    __validate_node_attributes('node_shape.diag', shape=shapes)
+
+
+def test_node_shape_namespace_diagram():
+    shapes = {'A': 'flowchart.condition', 'B': 'condition', 'Z': 'box'}
+    __validate_node_attributes('node_shape_namespace.diag', shape=shapes)
+
+
+def test_node_has_multilined_label_diagram():
+    positions = {'A': (0, 0), 'Z': (0, 1)}
+    labels = {'A': "foo\nbar", 'Z': 'Z'}
+    __validate_node_attributes('node_has_multilined_label.diag',
+                               xy=positions, label=labels)
+
+
+def test_quoted_node_id_diagram():
+    positions = {'A': (0, 0), "'A'": (1, 0), 'B': (2, 0), 'Z': (0, 1)}
+    __validate_node_attributes('quoted_node_id.diag', xy=positions)
+
+
+def test_node_id_includes_dot_diagram():
+    positions = {'A.B': (0, 0), 'C.D': (1, 0), 'Z': (0, 1)}
+    __validate_node_attributes('node_id_includes_dot.diag', xy=positions)
+
+
+def test_multiple_node_relation_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1),
+                 'D': (2, 0), 'Z': (0, 2)}
+    __validate_node_attributes('multiple_node_relation.diag', xy=positions)
+
+
+def test_node_attribute():
+    labels = {'A': 'B', 'B': 'double quoted', 'C': 'single quoted',
+              'D': '\'"double" quoted\'', 'E': '"\'single\' quoted"',
+              'F': 'F', 'G': 'G'}
+    colors = {'A': (255, 0, 0), 'B': (255, 255, 255), 'C': (255, 0, 0),
+              'D': (255, 0, 0), 'E': (255, 0, 0), 'F': (255, 255, 255),
+              'G': (255, 255, 255)}
+    textcolors = {'A': (0, 0, 0), 'B': (0, 0, 0), 'C': (0, 0, 0),
+                  'D': (0, 0, 0), 'E': (0, 0, 0), 'F': (255, 0, 0),
+                  'G': (0, 0, 0)}
+    numbered = {'A': None, 'B': None, 'C': None, 'D': None,
+                'E': '1', 'F': None, 'G': None}
+    stacked = {'A': False, 'B': False, 'C': False, 'D': False,
+               'E': False, 'F': False, 'G': True}
+    __validate_node_attributes('node_attribute.diag', label=labels,
+                               color=colors, textcolor=textcolors,
+                               numbered=numbered, stacked=stacked)
+
+
+def test_node_height_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0),
+                 'D': (2, 1), 'E': (1, 1), 'Z': (0, 2)}
+    __validate_node_attributes('node_height.diag', xy=positions)
+
+
+def test_branched_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0),
+                 'D': (1, 1), 'E': (2, 1), 'Z': (0, 2)}
+    __validate_node_attributes('branched.diag', xy=positions)
+
+
+def test_multiple_parent_node_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (0, 2),
+                 'D': (1, 2), 'E': (0, 1), 'Z': (0, 3)}
+    __validate_node_attributes('multiple_parent_node.diag', xy=positions)
+
+
+def test_twin_multiple_parent_node_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (0, 1),
+                 'D': (1, 1), 'E': (0, 2), 'Z': (0, 3)}
+    __validate_node_attributes('twin_multiple_parent_node.diag', xy=positions)
+
+
+def test_flowable_node_diagram():
+    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0), 'Z': (0, 1)}
+    __validate_node_attributes('flowable_node.diag', xy=positions)
