1a84bca442235c72e0a4d0896e2d6a92b05a228c
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-10-05 18:21:50 +0900

    * Replace SVG core by original's (simplesvg.py)

--- src/blockdiag/noderenderer/flowchart/terminator.py
+++ src/blockdiag/noderenderer/flowchart/terminator.py
@@ -16,7 +16,7 @@
 from blockdiag.noderenderer import NodeShape
 from blockdiag.noderenderer import install_renderer
 from blockdiag.utils.XY import XY
-from blockdiag.imagedraw.SVGdraw import pathdata
+from blockdiag.imagedraw.simplesvg import pathdata
 
 
 class Terminator(NodeShape):
