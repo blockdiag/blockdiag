2f24e7edd7de886bbaa01f1c199acc17edb0f5b2
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-10-06 12:10:52 +0900

    * Add testcases for skipped_edge

--- src/blockdiag/tests/utils.py
+++ src/blockdiag/tests/utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 from nose.tools import eq_
 from blockdiag.builder import *
 from blockdiag.diagparser import parse_string
@@ -18,8 +19,23 @@ def __validate_node_attributes(filename, **kwargs):
     diagram = __build_diagram(filename)
 
     for name, values in kwargs.items():
-        print "[node.%s]" % name
-        for node in (n for n in diagram.nodes  if n.drawable):
-            print node
-            value = getattr(node, name)
-            eq_(values[node.id], value)
+        if re.match('edge_', name):
+            print "[%s]" % name
+            name = re.sub('edge_', '', name)
+            for (id1, id2), value in values.items():
+                found = False
+                for edge in diagram.edges:
+                    if edge.node1.id == id1 and edge.node2.id == id2:
+                        print edge
+                        eq_(value, getattr(edge, name))
+                        found = True
+
+                if not found:
+                    raise RuntimeError('edge (%s -> %s) is not found' % \
+                                       (id1, id2))
+        else:
+            print "[node.%s]" % name
+            for node in (n for n in diagram.nodes  if n.drawable):
+                print node
+                value = getattr(node, name)
+                eq_(values[node.id], value)
