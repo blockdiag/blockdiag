94edf9d2964854cc6a3f8641f17769e1767819cb
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-11-13 19:57:06 +0900

    Add outputdir parameter to rst.directives.setup()

--- src/blockdiag/tests/test_rst_directives.py
+++ src/blockdiag/tests/test_rst_directives.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import re
+import os
 import sys
 import unittest
+import tempfile
 from utils import stderr_wrapper
+from nose.tools import raises
 from docutils import nodes
 from docutils.core import publish_doctree
 from docutils.parsers.rst import directives as docutils
@@ -14,6 +18,20 @@ def setup_directive_base(func):
         klass = directives.BlockdiagDirectiveBase
         docutils.register_directive('blockdiag', klass)
         func(self)
+
+    _.__name__ = func.__name__
+    return _
+
+
+def use_tmpdir(func):
+    def _(self):
+        try:
+            tmpdir = tempfile.mkdtemp()
+            func(self, tmpdir)
+        finally:
+            for file in os.listdir(tmpdir):
+                os.unlink(tmpdir + "/" + file)
+            os.rmdir(tmpdir)
 
     _.__name__ = func.__name__
     return _
@@ -49,13 +67,15 @@ class TestRstDirectives(unittest.TestCase):
     def test_rst_directives_base_noargs(self):
         text = ".. blockdiag::"
         doctree = publish_doctree(text)
+        self.assertEqual(1, len(doctree))
         self.assertEqual(type(doctree[0]), nodes.system_message)
 
     @setup_directive_base
     def test_rst_directives_base_with_block(self):
         text = ".. blockdiag::\n\n   { A -> B }"
         doctree = publish_doctree(text)
-        self.assertEqual(type(doctree[0]), directives.blockdiag)
+        self.assertEqual(1, len(doctree))
+        self.assertEqual(directives.blockdiag, type(doctree[0]))
         self.assertEqual('{ A -> B }', doctree[0]['code'])
         self.assertEqual(None, doctree[0]['alt'])
         self.assertEqual({}, doctree[0]['options'])
@@ -65,6 +85,7 @@ class TestRstDirectives(unittest.TestCase):
     def test_rst_directives_base_with_emptyblock(self):
         text = ".. blockdiag::\n\n   \n"
         doctree = publish_doctree(text)
+        self.assertEqual(1, len(doctree))
         self.assertEqual(type(doctree[0]), nodes.system_message)
 
     # FIXME
@@ -79,6 +100,7 @@ class TestRstDirectives(unittest.TestCase):
     def test_rst_directives_base_with_block_and_filename(self):
         text = ".. blockdiag:: diagrams/node_attributes.diag\n\n   { A -> B }"
         doctree = publish_doctree(text)
+        self.assertEqual(1, len(doctree))
         self.assertEqual(type(doctree[0]), nodes.system_message)
 
     @setup_directive_base
@@ -86,8 +108,53 @@ class TestRstDirectives(unittest.TestCase):
         text = ".. blockdiag::\n   :alt: hello world\n   :desctable:\n" + \
                "   :maxwidth: 100\n\n   { A -> B }"
         doctree = publish_doctree(text)
-        self.assertEqual(type(doctree[0]), directives.blockdiag)
+        self.assertEqual(1, len(doctree))
+        self.assertEqual(directives.blockdiag, type(doctree[0]))
         self.assertEqual('{ A -> B }', doctree[0]['code'])
         self.assertEqual('hello world', doctree[0]['alt'])
         self.assertEqual(None, doctree[0]['options']['desctable'])
         self.assertEqual(100, doctree[0]['options']['maxwidth'])
+
+    @use_tmpdir
+    def test_rst_directives_with_block(self, path):
+        directives.setup(format='SVG', outputdir=path)
+        text = ".. blockdiag::\n\n   { A -> B }"
+        doctree = publish_doctree(text)
+        self.assertEqual(type(doctree[0]), nodes.image)
+        self.assertFalse('alt' in doctree[0]) 
+        self.assertEqual(0, doctree[0]['uri'].index(path))
+        self.assertFalse('target' in doctree[0]) 
+
+    @use_tmpdir
+    def test_rst_directives_with_block_alt(self, path):
+        directives.setup(format='SVG', outputdir=path)
+        text = ".. blockdiag::\n   :alt: hello world\n\n   { A -> B }"
+        doctree = publish_doctree(text)
+        self.assertEqual(type(doctree[0]), nodes.image)
+        self.assertEqual('hello world', doctree[0]['alt'])
+        self.assertEqual(0, doctree[0]['uri'].index(path))
+        self.assertFalse('target' in doctree[0]) 
+
+    @raises(RuntimeError)
+    @use_tmpdir
+    def test_rst_directives_with_block_fontpath1(self, path):
+        directives.setup(format='SVG', fontpath=['dummy.ttf'], outputdir=path)
+        text = ".. blockdiag::\n   :alt: hello world\n\n   { A -> B }"
+        doctree = publish_doctree(text)
+
+    @raises(RuntimeError)
+    @use_tmpdir
+    def test_rst_directives_with_block_fontpath2(self, path):
+        directives.setup(format='SVG', fontpath='dummy.ttf', outputdir=path)
+        text = ".. blockdiag::\n   :alt: hello world\n\n   { A -> B }"
+        doctree = publish_doctree(text)
+
+    @use_tmpdir
+    def test_rst_directives_with_block_maxwidth(self, path):
+        directives.setup(format='SVG', outputdir=path)
+        text = ".. blockdiag::\n   :maxwidth: 100\n\n   { A -> B }"
+        doctree = publish_doctree(text)
+        self.assertEqual(type(doctree[0]), nodes.image)
+        self.assertFalse('alt' in doctree[0]) 
+        self.assertEqual(0, doctree[0]['uri'].index(path))
+        self.assertFalse(0, doctree[0]['target'].index(path)) 
