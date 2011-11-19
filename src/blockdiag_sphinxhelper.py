86f7da67c60174513c891a890d8a2b26855d9039
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-11-19 18:27:20 +0900

    Update sphinxhelper

--- src/blockdiag_sphinxhelper.py
+++ src/blockdiag_sphinxhelper.py
@@ -13,5 +13,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from blockdiag import diagparser, builder, DiagramDraw
+from blockdiag import command, diagparser, builder, DiagramDraw
+from blockdiag.utils import collections
+from blockdiag.utils.fontmap import FontMap
 from blockdiag.utils.rst.directives import blockdiag, BlockdiagDirective
