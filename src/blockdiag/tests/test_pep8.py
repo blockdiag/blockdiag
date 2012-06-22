35e86bdac12678827e23972b726ad6818566ac42
tk0miya <i.tkomiya@gmail.com>
2012-06-22 12:07:47 +0900

    Use new pep8 API

--- src/blockdiag/tests/test_pep8.py
+++ src/blockdiag/tests/test_pep8.py
@@ -8,31 +8,43 @@ BASE_DIR = os.path.dirname(CURRENT_DIR)
 
 
 def test_pep8():
-    arglist = [
-        '--statistics',
-        '--filename=*.py',
-        '--show-source',
-        '--repeat',
-        '--exclude=SVGdraw.py',
-        #'--show-pep8',
-        #'-qq',
-        #'-v',
-        BASE_DIR,
-    ]
+    arglist = [['statistics', True],
+               ['show-source', True],
+               ['repeat', True],
+               ['ignore', 'E501'],
+               ['exclude', ['gen-py', 'GAuth.py', 'ttypes.py', 'constants.py']],
+               ['paths', [BASE_DIR]]]
 
-    options, args = pep8.process_options(arglist)
-    runner = pep8.input_file
+    pep8style = pep8.StyleGuide(arglist, parse_argv=False, config_file=True)
+    options = pep8style.options
+    if options.doctest:
+        import doctest
+        fail_d, done_d = doctest.testmod(report=False, verbose=options.verbose)
+        fail_s, done_s = selftest(options)
+        count_failed = fail_s + fail_d
+        if not options.quiet:
+            count_passed = done_d + done_s - count_failed
+            print("%d passed and %d failed." % (count_passed, count_failed))
+            print("Test failed." if count_failed else "Test passed.")
+        if count_failed:
+            sys.exit(1)
+    if options.testsuite:
+        init_tests(pep8style)
+    report = pep8style.check_files()
+    if options.statistics:
+        report.print_statistics()
+    if options.benchmark:
+        report.print_benchmark()
+    if options.testsuite and not options.quiet:
+        report.print_results()
+    if report.total_errors:
+        if options.count:
+            sys.stderr.write(str(report.total_errors) + '\n')
+        sys.exit(1)
 
-    for path in args:
-        if os.path.isdir(path):
-            pep8.input_dir(path, runner=runner)
-        elif not pep8.excluded(path):
-            options.counters['files'] += 1
-            runner(path)
-
-    pep8.print_statistics()
-    errors = pep8.get_count('E')
-    warnings = pep8.get_count('W')
+    # reporting errors (additional summary)
+    errors = report.get_count('E')
+    warnings = report.get_count('W')
     message = 'pep8: %d errors / %d warnings' % (errors, warnings)
     print message
-    assert errors + warnings == 0, message
+    assert report.total_errors == 0, message
