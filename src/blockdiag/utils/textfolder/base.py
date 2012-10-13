465bedc960f5f864e979b753dd08f7198a372498
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2012-10-13 12:04:15 +0900

    Use Box class as return value at TextFolder#outlinebox()

--- src/blockdiag/utils/textfolder/base.py
+++ src/blockdiag/utils/textfolder/base.py
@@ -213,7 +213,7 @@ class TextFolder(object):
                       max(p.x for p in corners) + self.padding,
                       max(p.y for p in corners) + self.line_spacing)
         else:
-            box = [self.box[0], self.box[1], self.box[0], self.box[1]]
+            box = Box(self.box[0], self.box[1], self.box[0], self.box[1])
 
         return box
 
