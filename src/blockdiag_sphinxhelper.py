0cd6268aab82225b3795a09209c78de32b4d95dc
tk0miya <i.tkomiya@gmail.com>
2012-11-17 16:11:43 +0900

    Update interface for sphinxcontrib module

--- src/blockdiag_sphinxhelper.py
+++ src/blockdiag_sphinxhelper.py
@@ -13,6 +13,20 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import blockdiag.parser
+import blockdiag.builder
+import blockdiag.drawer
+core = blockdiag
+
+import blockdiag.utils.bootstrap
+import blockdiag.utils.collections
+import blockdiag.utils.fontmap
+utils = blockdiag.utils
+
+from blockdiag.utils.rst import nodes
+from blockdiag.utils.rst import directives
+
+# FIXME: obsoleted interface (keep for compatibility)
 from blockdiag import command, parser, builder, drawer
 from blockdiag.utils import collections
 from blockdiag.utils.fontmap import FontMap
