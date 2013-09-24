b66c95ca9e123fd87c4aa0363325b9ffd248cc3d
tk0miya <i.tkomiya@gmail.com>
2013-09-24 18:33:16 +0900

    Refactor testcases: Replace node.tools by unittest.TestCase

--- src/blockdiag/tests/test_builder_node.py
+++ src/blockdiag/tests/test_builder_node.py
@@ -1,137 +1,139 @@
 # -*- coding: utf-8 -*-
 
 from collections import defaultdict
-from blockdiag.tests.utils import __build_diagram, __validate_node_attributes
+from blockdiag.tests.utils import BuilderTestCase
 
 
-def test_single_node_diagram():
-    diagram = __build_diagram('single_node.diag')
+class TestBuilderNode(BuilderTestCase):
+    def test_single_node_diagram(self):
+        diagram = self.build('single_node.diag')
 
-    assert len(diagram.nodes) == 1
-    assert len(diagram.edges) == 0
-    assert diagram.nodes[0].label == 'A'
-    assert diagram.nodes[0].xy == (0, 0)
+        self.assertEqual(1, len(diagram.nodes))
+        self.assertEqual(0, len(diagram.edges))
+        self.assertEqual('A', diagram.nodes[0].label)
+        self.assertEqual((0, 0), diagram.nodes[0].xy)
 
+    def test_node_shape_diagram(self):
+        expected = {'A': 'box', 'B': 'roundedbox', 'C': 'diamond',
+                    'D': 'ellipse', 'E': 'note', 'F': 'cloud',
+                    'G': 'mail', 'H': 'beginpoint', 'I': 'endpoint',
+                    'J': 'minidiamond', 'K': 'flowchart.condition',
+                    'L': 'flowchart.database', 'M': 'flowchart.input',
+                    'N': 'flowchart.loopin', 'O': 'flowchart.loopout',
+                    'P': 'actor', 'Q': 'flowchart.terminator', 'R': 'textbox',
+                    'S': 'dots', 'T': 'none', 'U': 'square', 'V': 'circle',
+                    'Z': 'box'}
+        diagram = self.build('node_shape.diag')
+        self.assertNodeShape(diagram, expected)
 
-def test_node_shape_diagram():
-    shapes = {'A': 'box', 'B': 'roundedbox', 'C': 'diamond',
-              'D': 'ellipse', 'E': 'note', 'F': 'cloud',
-              'G': 'mail', 'H': 'beginpoint', 'I': 'endpoint',
-              'J': 'minidiamond', 'K': 'flowchart.condition',
-              'L': 'flowchart.database', 'M': 'flowchart.input',
-              'N': 'flowchart.loopin', 'O': 'flowchart.loopout',
-              'P': 'actor', 'Q': 'flowchart.terminator', 'R': 'textbox',
-              'S': 'dots', 'T': 'none', 'U': 'square', 'V': 'circle',
-              'Z': 'box'}
-    __validate_node_attributes('node_shape.diag', shape=shapes)
+    def test_node_shape_namespace_diagram(self):
+        diagram = self.build('node_shape_namespace.diag')
+        self.assertNodeShape(diagram, {'A': 'flowchart.condition',
+                                       'B': 'condition',
+                                       'Z': 'box'})
 
+    def test_node_has_multilined_label_diagram(self):
+        diagram = self.build('node_has_multilined_label.diag')
+        self.assertNodeXY(diagram, {'A': (0, 0), 'Z': (0, 1)})
+        self.assertNodeLabel(diagram, {'A': "foo\nbar", 'Z': 'Z'})
 
-def test_node_shape_namespace_diagram():
-    shapes = {'A': 'flowchart.condition', 'B': 'condition', 'Z': 'box'}
-    __validate_node_attributes('node_shape_namespace.diag', shape=shapes)
+    def test_quoted_node_id_diagram(self):
+        diagram = self.build('quoted_node_id.diag')
+        self.assertNodeXY(diagram, {'A': (0, 0), "'A'": (1, 0),
+                                    'B': (2, 0), 'Z': (0, 1)})
 
+    def test_node_id_includes_dot_diagram(self):
+        diagram = self.build('node_id_includes_dot.diag')
+        self.assertNodeXY(diagram, {'A.B': (0, 0), 'C.D': (1, 0),
+                                    'Z': (0, 1)})
 
-def test_node_has_multilined_label_diagram():
-    positions = {'A': (0, 0), 'Z': (0, 1)}
-    labels = {'A': "foo\nbar", 'Z': 'Z'}
-    __validate_node_attributes('node_has_multilined_label.diag',
-                               xy=positions, label=labels)
+    def test_multiple_nodes_definition_diagram(self):
+        diagram = self.build('multiple_nodes_definition.diag')
+        self.assertNodeXY(diagram, {'A': (0, 0), 'B': (0, 1),
+                                    'Z': (0, 2)})
+        self.assertNodeColor(diagram, {'A': (255, 0, 0), 'B': (255, 0, 0),
+                                       'Z': (255, 255, 255)})
 
+    def test_multiple_node_relation_diagram(self):
+        diagram = self.build('multiple_node_relation.diag')
+        self.assertNodeXY(diagram, {'A': (0, 0), 'B': (1, 0),
+                                    'C': (1, 1), 'D': (2, 0),
+                                    'Z': (0, 2)})
 
-def test_quoted_node_id_diagram():
-    positions = {'A': (0, 0), "'A'": (1, 0), 'B': (2, 0), 'Z': (0, 1)}
-    __validate_node_attributes('quoted_node_id.diag', xy=positions)
+    def test_node_attribute(self):
+        labels = {'A': 'B', 'B': 'double quoted', 'C': 'single quoted',
+                  'D': '\'"double" quoted\'', 'E': '"\'single\' quoted"',
+                  'F': 'F', 'G': 'G', 'H': 'H', 'I': 'I', 'J': 'Hello'}
+        colors = {'A': (255, 0, 0), 'B': (255, 255, 255), 'C': (255, 0, 0),
+                  'D': (255, 0, 0), 'E': (255, 0, 0), 'F': (255, 255, 255),
+                  'G': (255, 255, 255), 'H': (255, 255, 255),
+                  'I': (255, 255, 255), 'J': (255, 255, 255)}
+        textcolors = defaultdict(lambda: (0, 0, 0))
+        textcolors['F'] = (255, 0, 0)
+        linecolors = defaultdict(lambda: (0, 0, 0))
+        linecolors['I'] = (255, 0, 0)
+        numbered = defaultdict(lambda: None)
+        numbered['E'] = '1'
+        stacked = defaultdict(lambda: False)
+        stacked['G'] = True
+        fontsize = defaultdict(lambda: None)
+        fontsize['H'] = 16
+        orientations = defaultdict(lambda: 'horizontal')
+        orientations['J'] = 'vertical'
 
+        diagram = self.build('node_attribute.diag')
+        self.assertNodeLabel(diagram, labels)
+        self.assertNodeColor(diagram, colors)
+        self.assertNodeTextColor(diagram, textcolors)
+        self.assertNodeLineColor(diagram, linecolors)
+        self.assertNodeNumbered(diagram, numbered)
+        self.assertNodeStacked(diagram, stacked)
+        self.assertNodeFontsize(diagram, fontsize)
+        self.assertNodeLabel_Orientation(diagram, orientations)
 
-def test_node_id_includes_dot_diagram():
-    positions = {'A.B': (0, 0), 'C.D': (1, 0), 'Z': (0, 1)}
-    __validate_node_attributes('node_id_includes_dot.diag', xy=positions)
+    def test_node_height_diagram(self):
+        diagram = self.build('node_height.diag')
+        self.assertNodeXY(diagram, {'A': (0, 0), 'B': (1, 0),
+                                    'C': (2, 0), 'D': (2, 1),
+                                    'E': (1, 1), 'Z': (0, 2)})
 
+    def test_branched_diagram(self):
+        diagram = self.build('branched.diag')
+        self.assertNodeXY(diagram, {'A': (0, 0), 'B': (1, 0),
+                                    'C': (2, 0), 'D': (1, 1),
+                                    'E': (2, 1), 'Z': (0, 2)})
 
-def test_multiple_nodes_definition_diagram():
-    positions = {'A': (0, 0), 'B': (0, 1), 'Z': (0, 2)}
-    colors = {'A': (255, 0, 0), 'B': (255, 0, 0), 'Z': (255, 255, 255)}
-    __validate_node_attributes('multiple_nodes_definition.diag', xy=positions,
-                               color=colors)
+    def test_multiple_parent_node_diagram(self):
+        diagram = self.build('multiple_parent_node.diag')
+        self.assertNodeXY(diagram, {'A': (0, 0), 'B': (1, 0),
+                                    'C': (0, 2), 'D': (1, 2),
+                                    'E': (0, 1), 'Z': (0, 3)})
 
+    def test_twin_multiple_parent_node_diagram(self):
+        diagram = self.build('twin_multiple_parent_node.diag')
+        self.assertNodeXY(diagram, {'A': (0, 0), 'B': (1, 0),
+                                    'C': (0, 1), 'D': (1, 1),
+                                    'E': (0, 2), 'Z': (0, 3)})
 
-def test_multiple_node_relation_diagram():
-    positions = {'A': (0, 0), 'B': (1, 0), 'C': (1, 1),
-                 'D': (2, 0), 'Z': (0, 2)}
-    __validate_node_attributes('multiple_node_relation.diag', xy=positions)
+    def test_flowable_node_diagram(self):
+        diagram = self.build('flowable_node.diag')
+        self.assertNodeXY(diagram, {'A': (0, 0), 'B': (1, 0),
+                                    'C': (2, 0), 'Z': (0, 1)})
 
+    def test_plugin_autoclass_diagram(self):
+        diagram = self.build('plugin_autoclass.diag')
+        self.assertNodeXY(diagram, {'A_emphasis': (0, 0),
+                                    'B_emphasis': (1, 0),
+                                    'C': (1, 1)})
+        self.assertNodeStyle(diagram, {'A_emphasis': 'dashed',
+                                       'B_emphasis': 'dashed',
+                                       'C': None})
+        self.assertNodeColor(diagram, {'A_emphasis': (255, 0, 0),
+                                       'B_emphasis': (255, 0, 0),
+                                       'C': (255, 255, 255)})
 
-def test_node_attribute():
-    labels = {'A': 'B', 'B': 'double quoted', 'C': 'single quoted',
-              'D': '\'"double" quoted\'', 'E': '"\'single\' quoted"',
-              'F': 'F', 'G': 'G', 'H': 'H', 'I': 'I', 'J': 'Hello'}
-    colors = {'A': (255, 0, 0), 'B': (255, 255, 255), 'C': (255, 0, 0),
-              'D': (255, 0, 0), 'E': (255, 0, 0), 'F': (255, 255, 255),
-              'G': (255, 255, 255), 'H': (255, 255, 255), 'I': (255, 255, 255),
-              'J': (255, 255, 255)}
-    textcolors = defaultdict(lambda: (0, 0, 0))
-    textcolors['F'] = (255, 0, 0)
-    numbered = defaultdict(lambda: None)
-    numbered['E'] = '1'
-    stacked = defaultdict(lambda: False)
-    stacked['G'] = True
-    fontsize = defaultdict(lambda: None)
-    fontsize['H'] = 16
-    linecolors = defaultdict(lambda: (0, 0, 0))
-    linecolors['I'] = (255, 0, 0)
-    orientations = defaultdict(lambda: 'horizontal')
-    orientations['J'] = 'vertical'
-
-    __validate_node_attributes('node_attribute.diag', label=labels,
-                               color=colors, textcolor=textcolors,
-                               numbered=numbered, stacked=stacked,
-                               fontsize=fontsize, linecolor=linecolors,
-                               label_orientation=orientations)
-
-
-def test_node_height_diagram():
-    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0),
-                 'D': (2, 1), 'E': (1, 1), 'Z': (0, 2)}
-    __validate_node_attributes('node_height.diag', xy=positions)
-
-
-def test_branched_diagram():
-    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0),
-                 'D': (1, 1), 'E': (2, 1), 'Z': (0, 2)}
-    __validate_node_attributes('branched.diag', xy=positions)
-
-
-def test_multiple_parent_node_diagram():
-    positions = {'A': (0, 0), 'B': (1, 0), 'C': (0, 2),
-                 'D': (1, 2), 'E': (0, 1), 'Z': (0, 3)}
-    __validate_node_attributes('multiple_parent_node.diag', xy=positions)
-
-
-def test_twin_multiple_parent_node_diagram():
-    positions = {'A': (0, 0), 'B': (1, 0), 'C': (0, 1),
-                 'D': (1, 1), 'E': (0, 2), 'Z': (0, 3)}
-    __validate_node_attributes('twin_multiple_parent_node.diag', xy=positions)
-
-
-def test_flowable_node_diagram():
-    positions = {'A': (0, 0), 'B': (1, 0), 'C': (2, 0), 'Z': (0, 1)}
-    __validate_node_attributes('flowable_node.diag', xy=positions)
-
-
-def test_plugin_autoclass_diagram():
-    positions = {'A_emphasis': (0, 0), 'B_emphasis': (1, 0), 'C': (1, 1)}
-    styles = {'A_emphasis': 'dashed', 'B_emphasis': 'dashed', 'C': None}
-    colors = {'A_emphasis': (255, 0, 0), 'B_emphasis': (255, 0, 0),
-              'C': (255, 255, 255)}
-
-    __validate_node_attributes('plugin_autoclass.diag', xy=positions,
-                               style=styles, color=colors)
-
-
-def test_plugin_attributes_diagram():
-    attr1 = {'A': "1", 'B': None}
-    attr2 = {'A': "2", 'B': None}
-    attr3 = {'A': "3", 'B': None}
-
-    __validate_node_attributes('plugin_attributes.diag', test_attr1=attr1,
-                               test_attr2=attr2, test_attr3=attr3)
+    def test_plugin_attributes_diagram(self):
+        diagram = self.build('plugin_attributes.diag')
+        self.assertNodeTest_Attr1(diagram, {'A': "1", 'B': None})
+        self.assertNodeTest_Attr2(diagram, {'A': "2", 'B': None})
+        self.assertNodeTest_Attr3(diagram, {'A': "3", 'B': None})
--- src/blockdiag/tests/test_parser.py
+++ src/blockdiag/tests/test_parser.py
@@ -1,142 +1,139 @@
 # -*- coding: utf-8 -*-
-
 from __future__ import print_function
+
+import sys
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
 from blockdiag.parser import parse_string, ParseException
 from blockdiag.parser import Graph, SubGraph, Statements, Node, Edge
-from nose.tools import raises
 
 
-def test_parser_basic():
-    # basic digram
-    code = """
-           diagram test {
-              A -> B -> C, D;
-           }
-           """
+class TestParser(unittest.TestCase):
+    def test_basic(self):
+        # basic digram
+        code = """
+               diagram test {
+                  A -> B -> C, D;
+               }
+               """
 
-    tree = parse_string(code)
-    assert isinstance(tree, Graph)
+        tree = parse_string(code)
+        self.assertIsInstance(tree, Graph)
 
+    def test_without_diagram_id(self):
+        code = """
+               diagram {
+                  A -> B -> C, D;
+               }
+               """
+        tree = parse_string(code)
+        self.assertIsInstance(tree, Graph)
 
-def test_parser_without_diagram_id():
-    code = """
-           diagram {
-              A -> B -> C, D;
-           }
-           """
-    tree = parse_string(code)
-    assert isinstance(tree, Graph)
+        code = """
+               {
+                  A -> B -> C, D;
+               }
+               """
+        tree = parse_string(code)
+        self.assertIsInstance(tree, Graph)
 
-    code = """
-           {
-              A -> B -> C, D;
-           }
-           """
-    tree = parse_string(code)
-    assert isinstance(tree, Graph)
+    def test_empty_diagram(self):
+        code = """
+               diagram {
+               }
+               """
+        tree = parse_string(code)
+        self.assertIsInstance(tree, Graph)
 
+        code = """
+               {
+               }
+               """
+        tree = parse_string(code)
+        self.assertIsInstance(tree, Graph)
 
-def test_parser_empty_diagram():
-    code = """
-           diagram {
-           }
-           """
-    tree = parse_string(code)
-    assert isinstance(tree, Graph)
+    def test_diagram_includes_nodes(self):
+        code = """
+               diagram {
+                 A;
+                 B [label = "foobar"];
+                 C [color = "red"];
+               }
+               """
+        tree = parse_string(code)
+        self.assertIsInstance(tree, Graph)
+        self.assertEqual(3, len(tree.stmts))
+        self.assertIsInstance(tree.stmts[0], Statements)
+        self.assertIsInstance(tree.stmts[0].stmts[0], Node)
+        self.assertIsInstance(tree.stmts[1], Statements)
+        self.assertIsInstance(tree.stmts[1].stmts[0], Node)
+        self.assertIsInstance(tree.stmts[2], Statements)
+        self.assertIsInstance(tree.stmts[2].stmts[0], Node)
 
-    code = """
-           {
-           }
-           """
-    tree = parse_string(code)
-    assert isinstance(tree, Graph)
+    def test_diagram_includes_edges(self):
+        code = """
+               diagram {
+                 A -> B -> C;
+               }
+               """
+        tree = parse_string(code)
+        self.assertIsInstance(tree, Graph)
+        self.assertEqual(1, len(tree.stmts))
+        self.assertIsInstance(tree.stmts[0], Edge)
 
+        code = """
+               diagram {
+                 A -> B -> C [style = dotted];
+                 D -> E, F;
+               }
+               """
+        tree = parse_string(code)
+        self.assertIsInstance(tree, Graph)
+        self.assertEqual(2, len(tree.stmts))
+        self.assertIsInstance(tree.stmts[0], Edge)
+        self.assertIsInstance(tree.stmts[1], Edge)
 
-def test_parser_diagram_includes_nodes():
-    code = """
-           diagram {
-             A;
-             B [label = "foobar"];
-             C [color = "red"];
-           }
-           """
-    tree = parse_string(code)
-    assert isinstance(tree, Graph)
-    assert len(tree.stmts) == 3
-    assert isinstance(tree.stmts[0], Statements)
-    assert isinstance(tree.stmts[0].stmts[0], Node)
-    assert isinstance(tree.stmts[1], Statements)
-    assert isinstance(tree.stmts[1].stmts[0], Node)
-    assert isinstance(tree.stmts[2], Statements)
-    assert isinstance(tree.stmts[2].stmts[0], Node)
+    def test_diagram_includes_groups(self):
+        code = """
+               diagram {
+                 group {
+                   A; B;
+                 }
+                 group {
+                   C -> D;
+                 }
+               }
+               """
+        tree = parse_string(code)
+        self.assertIsInstance(tree, Graph)
+        self.assertEqual(2, len(tree.stmts))
 
+        self.assertIsInstance(tree.stmts[0], SubGraph)
+        self.assertEqual(2, len(tree.stmts[0].stmts))
+        self.assertIsInstance(tree.stmts[0].stmts[0], Statements)
+        self.assertIsInstance(tree.stmts[0].stmts[0].stmts[0], Node)
+        self.assertIsInstance(tree.stmts[0].stmts[1], Statements)
+        self.assertIsInstance(tree.stmts[0].stmts[1].stmts[0], Node)
 
-def test_parser_diagram_includes_edges():
-    code = """
-           diagram {
-             A -> B -> C;
-           }
-           """
-    tree = parse_string(code)
-    assert isinstance(tree, Graph)
-    print(tree.stmts)
-    assert len(tree.stmts) == 1
-    assert isinstance(tree.stmts[0], Edge)
+        self.assertIsInstance(tree.stmts[1], SubGraph)
+        self.assertEqual(1, len(tree.stmts[1].stmts))
+        self.assertIsInstance(tree.stmts[1].stmts[0], Edge)
 
-    code = """
-           diagram {
-             A -> B -> C [style = dotted];
-             D -> E, F;
-           }
-           """
-    tree = parse_string(code)
-    assert isinstance(tree, Graph)
-    print(tree.stmts)
-    assert len(tree.stmts) == 2
-    assert isinstance(tree.stmts[0], Edge)
-    assert isinstance(tree.stmts[1], Edge)
+    def test_diagram_includes_diagram_attributes(self):
+        code = """
+               diagram {
+                 fontsize = 12;
+                 node_width = 80;
+               }
+               """
+        tree = parse_string(code)
+        self.assertIsInstance(tree, Graph)
+        self.assertEqual(2, len(tree.stmts))
 
-
-def test_parser_diagram_includes_groups():
-    code = """
-           diagram {
-             group {
-               A; B;
-             }
-             group {
-               C -> D;
-             }
-           }
-           """
-    tree = parse_string(code)
-    assert isinstance(tree, Graph)
-    assert len(tree.stmts) == 2
-
-    assert isinstance(tree.stmts[0], SubGraph)
-    assert len(tree.stmts[0].stmts) == 2
-    assert isinstance(tree.stmts[0].stmts[0], Statements)
-    assert isinstance(tree.stmts[0].stmts[0].stmts[0], Node)
-    assert isinstance(tree.stmts[0].stmts[1], Statements)
-    assert isinstance(tree.stmts[0].stmts[1].stmts[0], Node)
-
-    assert isinstance(tree.stmts[1], SubGraph)
-    assert len(tree.stmts[1].stmts) == 1
-    assert isinstance(tree.stmts[1].stmts[0], Edge)
-
-
-def test_parser_diagram_includes_diagram_attributes():
-    code = """
-           diagram {
-             fontsize = 12;
-             node_width = 80;
-           }
-           """
-    tree = parse_string(code)
-    assert isinstance(tree, Graph)
-    assert len(tree.stmts) == 2
-
-
-@raises(ParseException)
-def test_parser_parenthesis_ness():
-    code = ""
-    parse_string(code)
+    def test_parenthesis_ness(self):
+        with self.assertRaises(ParseException):
+            code = ""
+            parse_string(code)
