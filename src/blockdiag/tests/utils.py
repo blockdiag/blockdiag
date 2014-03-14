789f2fb68f623ed5ec2b11b033bf23b67eb99e96
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2014-03-14 16:15:31 +0900

    Fix capturing stderr outputs in py2.x

--- src/blockdiag/tests/utils.py
+++ src/blockdiag/tests/utils.py
@@ -16,9 +16,11 @@ from blockdiag.builder import ScreenNodeBuilder
 from blockdiag.parser import parse_file
 
 try:
-    from io import StringIO
-except ImportError:
+    # sys.stderr in py2.x allows mixture of str and unicode
     from cStringIO import StringIO
+except ImportError:
+    # sys.stderr in py3.x allows only str objects (disallow bytes objs)
+    from io import StringIO
 
 
 def supported_pil():
