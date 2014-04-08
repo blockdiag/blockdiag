f3ad1468cf52a5e8b40b90ba83bbd5599d667451
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2014-04-08 15:57:38 +0900

    Install py34 in drone.io

--- .drone.io.sh
+++ .drone.io.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
-sudo apt-get install python2.6 python2.6-dev fonts-ipafont-gothic fonts-vlgothic libjpeg8-dev libfreetype6-dev
+sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev fonts-ipafont-gothic fonts-vlgothic libjpeg8-dev libfreetype6-dev
 
 mkdir src/blockdiag/tests/truetype
 cp /usr/share/fonts/truetype/vlgothic/VL-PGothic-Regular.ttf src/blockdiag/tests/truetype
