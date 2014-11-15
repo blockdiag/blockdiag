8d5e44b5af7443f361450a2e7ab121b37bc9b1f8
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2014-11-15 14:15:38 +0900

    Improve test skipper

--- .drone.io.sh
+++ .drone.io.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
-sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev fonts-ipafont-gothic fonts-vlgothic libjpeg8-dev libfreetype6-dev
-
-mkdir src/blockdiag/tests/truetype
-cp /usr/share/fonts/truetype/vlgothic/VL-PGothic-Regular.ttf src/blockdiag/tests/truetype
+sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev fonts-ipafont-gothic libjpeg8-dev libfreetype6-dev
 
 pip install --use-mirrors --upgrade detox misspellings check-manifest docutils
 find src/ -name "*.py" | misspellings -f -
-detox
+ALL_TESTS=1 detox
 check-manifest
