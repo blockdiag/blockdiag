8062b50b4876a6789abfec7b593a33338ad6e0ac
tk0miya <i.tkomiya@gmail.com>
2015-01-01 22:52:49 +0900

    Use wheel package at drone.io

--- .drone.io.sh
+++ .drone.io.sh
@@ -3,7 +3,8 @@ sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
 sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev fonts-ipafont-gothic libjpeg8-dev libfreetype6-dev
 
-pip install --use-mirrors --upgrade detox misspellings check-manifest docutils
+pip install --use-mirrors --upgrade wheel
+pip install --use-mirrors --upgrade --use-wheel detox misspellings check-manifest docutils
 find src/ -name "*.py" | misspellings -f -
 ALL_TESTS=1 detox
 check-manifest
