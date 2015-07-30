5f5b7923159241906d8b1bae53bfc5d3febfcbe7
tk0miya <i.tkomiya@gmail.com>
2015-07-30 11:22:13 +0900

    Fix drone.io.sh; --use-mirrors option was deprecated

--- .drone.io.sh
+++ .drone.io.sh
@@ -3,8 +3,8 @@ sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
 sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev fonts-ipafont-gothic libjpeg8-dev libfreetype6-dev
 
-pip install --use-mirrors --upgrade pip setuptools wheel
-pip install --use-mirrors --upgrade --use-wheel detox misspellings check-manifest docutils
+pip install --upgrade pip setuptools wheel
+pip install --upgrade --use-wheel detox misspellings check-manifest docutils
 find src/ -name "*.py" | misspellings -f -
 ALL_TESTS=1 detox
 check-manifest
